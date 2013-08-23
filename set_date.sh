de0c2a7c391f9aba7990edc7acd0b5aaff8fe816
Robert Nelson <robertcnelson@gmail.com>
2013-08-22 19:16:54 -0500

    date: use same format
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- set_date.sh
+++ set_date.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
 if [ -f /etc/timestamp ] ; then
-	systemdate=$(/bin/date -u "+%4Y%2m%2d%2H%2M")
+	systemdate=$(/bin/date --utc "+%4Y%2m%2d%2H%2M")
 	timestamp=$(cat /etc/timestamp)
 
-	if [ ${systemdate} -lt ${timestamp} ] ; then
+	if [ ${timestamp} -gt ${systemdate} ] ; then
 		/bin/date -u ${timestamp}
 	fi
 fi
