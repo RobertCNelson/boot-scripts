ac18395a66bf0a5be156468f764c3aaaf99a8914
Robert Nelson <robertcnelson@gmail.com>
2013-08-22 20:02:06 -0500

    rtc: should work now...
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- set_date.sh
+++ set_date.sh
@@ -5,7 +5,14 @@ if [ -f /etc/timestamp ] ; then
 	timestamp=$(cat /etc/timestamp)
 
 	if [ ${timestamp} -gt ${systemdate} ] ; then
-		/bin/date -u ${timestamp}
+		year=$(cat /etc/timestamp | cut -b 1-4)
+		month=$(cat /etc/timestamp | cut -b 5-6)
+		day=$(cat /etc/timestamp | cut -b 7-8)
+		hour=$(cat /etc/timestamp | cut -b 9-10)
+		min=$(cat /etc/timestamp | cut -b 11-12)
+
+		#/bin/date --utc -s "10/08/2008 11:37:23"
+		/bin/date --utc -s "${month}/${day}/${year} ${hour}:${min}:00"
 	fi
 fi
 
