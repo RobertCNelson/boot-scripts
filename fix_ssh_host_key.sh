a4ddada878cd4e8a21d0b127e0d06707bb5396ad
Robert Nelson <robertcnelson@gmail.com>
2013-08-30 15:00:12 -0500

    boot: regenerate ssh key when host does not match
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- fix_ssh_host_key.sh
+++ fix_ssh_host_key.sh
@@ -2,9 +2,11 @@
 
 if [ -f /etc/ssh/ssh_host_dsa_key.pub ] ; then
 	unset fix_ssh
-	cat /etc/ssh/ssh_host_dsa_key.pub | grep "@`cat /etc/hostname`" || fix_ssh=1
+	cat /etc/ssh/ssh_host_dsa_key.pub | grep "@`cat /etc/hostname`" >/dev/null 2>&1 || fix_ssh=1
 
 	if [ "${fix_ssh}" ] ; then
+		/etc/init.d/ssh stop
+		echo "Regenerating SSH host keys..."
 		rm -rf /etc/ssh/ssh_host_* || true
 		dpkg-reconfigure openssh-server
 	fi
