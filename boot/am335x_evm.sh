719dad2c04f5d14edcf594ed85b1de6dc7e1b70a
Robert Nelson <robertcnelson@gmail.com>
2014-01-22 10:13:20 -0600

    am335x_evm: dump ip address to /etc/issue
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- boot/am335x_evm.sh
+++ boot/am335x_evm.sh
@@ -56,7 +56,7 @@ sleep 1
 
 sed -i -e 's:DHCPD_ENABLED="no":#DHCPD_ENABLED="no":g' /etc/default/udhcpd
 #Distro default...
-deb_udhcpd=$(cat /etc/udhcpd.conf | grep Sample)
+deb_udhcpd=$(cat /etc/udhcpd.conf | grep Sample || true)
 if [ "${deb_udhcpd}" ] ; then
 	mv /etc/udhcpd.conf /etc/udhcpd.conf.bak
 fi
@@ -72,6 +72,22 @@ fi
 
 /sbin/ifconfig usb0 192.168.7.2 netmask 255.255.255.252
 /usr/sbin/udhcpd -S /etc/udhcpd.conf
+
+eth0_addr=$(ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
+usb0_addr=$(ip addr list usb0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
+wlan0_addr=$(ip addr list wlan0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
+
+sed -i -e '/Address/d' /etc/issue
+
+if [ ! "x${eth0_addr}" = "x" ] ; then
+	echo "The IP Address for eth0 is: ${eth0_addr}" >> /etc/issue
+fi
+if [ ! "x${wlan0_addr}" = "x" ] ; then
+	echo "The IP Address for wlan0 is: ${wlan0_addr}" >> /etc/issue
+fi
+if [ ! "x${usb0_addr}" = "x" ] ; then
+	echo "The IP Address for usb0 is: ${usb0_addr}" >> /etc/issue
+fi
 
 if [ -f /boot/uboot/flash-eMMC.txt ] ; then
 	if [ ! -d /boot/uboot/debug/ ] ; then
