21c1af2781ebb8797e60dee0cfa7ff08aaa64781
Robert Nelson <robertcnelson@gmail.com>
2014-01-31 12:37:15 -0600

    add: disable-bonescript-renable-apache-port-80.sh
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ un-tweak-image/disable-bonescript-renable-apache-port-80.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo systemctl disable bonescript.socket
+sudo sed -i -e 's:8080:80:g' /etc/apache2/ports.conf
+sudo sed -i -e 's:8080:80:g' /etc/apache2/sites-enabled/000-default
+sudo /etc/init.d/apache2 restart
