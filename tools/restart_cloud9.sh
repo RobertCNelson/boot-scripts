f0dd9e1aa4c23da25dbeb2ca270ee426ba74d3ac
Robert Nelson <robertcnelson@gmail.com>
2014-02-12 11:04:41 -0600

    cloud9
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/restart_cloud9.sh
+++ tools/restart_cloud9.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #/opt/cloud9/bin/cloud9.sh -l 0.0.0.0 -w /var/lib/cloud9 -p 3000 >/opt/cloud9/log 2>&1 &
-/usr/local/bin/node /opt/cloud9/server.js -l 0.0.0.0 -w /var/lib/cloud9 -p 3000 >/opt/cloud9/log 2>&1 &
+#/usr/local/bin/node /opt/cloud9/server.js -l 0.0.0.0 -w /var/lib/cloud9 -p 3000 >/opt/cloud9/log 2>&1 &
+/usr/bin/node /opt/cloud9/server.js -l 0.0.0.0 -w /var/lib/cloud9 -p 3000
