47fa29a9124c6bb9f9a112b69b0fb6bdc23bf450
Robert Nelson <robertcnelson@gmail.com>
2014-02-18 16:34:19 -0600

    cloud9: quick init start hack
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- boot/am335x_evm.sh
+++ boot/am335x_evm.sh
@@ -135,4 +135,11 @@ if [ -f /boot/uboot/resizerootfs ] || [ -f /resizerootfs ] ; then
 	rm -rf /boot/uboot/resizerootfs || true
 	rm -rf /resizerootfs || true
 fi
+
+#FIXME: need to move this to systemd, this is just temp..
+if [ -f /opt/scripts/tools/restart_cloud9.sh ] ; then
+	if [ -f /opt/cloud9/build/standalone/config ] ; then
+		/bin/bash /opt/scripts/tools/restart_cloud9.sh &> /opt/cloud9/log &
+	fi
+fi
 #
