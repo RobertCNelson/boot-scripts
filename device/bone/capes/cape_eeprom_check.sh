5d3f9782b76b43bf63995c2f85083021063972ce
Robert Nelson <robertcnelson@gmail.com>
2014-02-24 16:50:09 -0600

    cape: more debugging
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/bone/capes/cape_eeprom_check.sh
+++ device/bone/capes/cape_eeprom_check.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+echo "trying: /sys/bus/i2c/devices/1-0054/eeprom"
 cat /sys/bus/i2c/devices/1-0054/eeprom | hexdump -c
+
+echo "trying: /sys/bus/i2c/devices/1-0055/eeprom"
 cat /sys/bus/i2c/devices/1-0055/eeprom | hexdump -c
+
+echo "trying: /sys/bus/i2c/devices/1-0056/eeprom"
 cat /sys/bus/i2c/devices/1-0056/eeprom | hexdump -c
+
+echo "trying: /sys/bus/i2c/devices/1-0057/eeprom"
 cat /sys/bus/i2c/devices/1-0057/eeprom | hexdump -c
 
 echo "cat eeprom.dump > /sys/bus/i2c/devices/1-005X/eeprom"
