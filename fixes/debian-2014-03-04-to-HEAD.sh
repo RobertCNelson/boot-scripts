76919aa342a01c5d6b93c74015b67a5becf1aff8
Robert Nelson <robertcnelson@gmail.com>
2014-03-05 17:12:37 -0600

    debian-2014-03-04: add RT2870STA.dat fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ fixes/debian-2014-03-04-to-HEAD.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ -f /etc/Wireless/RT2870/RT2870STA.dat ] ; then
+	#fixed: https://github.com/rcn-ee/farm/commit/f5e66c19decc06954f00509913a4a2cdf1afd7bc
+	echo "MT601: Moving RT2870STA.dat to /etc/Wireless/RT2870STA/RT2870STA.dat"
+	sudo mv /etc/Wireless/RT2870/RT2870STA.dat /etc/Wireless/RT2870STA/RT2870STA.dat
+fi
+
