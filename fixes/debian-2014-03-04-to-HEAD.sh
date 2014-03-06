f97f355a085e1559c18c7055f600111390f7a3f2
Robert Nelson <robertcnelson@gmail.com>
2014-03-06 14:34:12 -0600

    debian-2014-03-04: add libopencv-dev
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- fixes/debian-2014-03-04-to-HEAD.sh
+++ fixes/debian-2014-03-04-to-HEAD.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-pkgs="libopencv-core-dev"
+check_dpkg () {
+	LC_ALL=C dpkg --list | awk '{print $2}' | grep "^${pkg}$" >/dev/null || deb_pkgs="${deb_pkgs}${pkg} "
+}
 
 if [ -f /etc/Wireless/RT2870/RT2870STA.dat ] ; then
 	#fixed: https://github.com/rcn-ee/farm/commit/f5e66c19decc06954f00509913a4a2cdf1afd7bc
@@ -9,6 +11,16 @@ if [ -f /etc/Wireless/RT2870/RT2870STA.dat ] ; then
 	sudo mv /etc/Wireless/RT2870/RT2870STA.dat /etc/Wireless/RT2870STA/RT2870STA.dat
 fi
 
-sudo apt-get update
-sudo apt-get -y install ${pkgs}
+unset deb_pkgs
+pkg="libopencv-dev"
+check_dpkg
+#fixed: https://github.com/beagleboard/image-builder/commit/6af879606f2638dda363c54264e8e72ddb032b98
+pkg="libopencv-core-dev"
+check_dpkg
+#fixed: https://github.com/beagleboard/image-builder/commit/1c0e5f8f272c6e3a3b659ea7e3854abe5c81df40
+
+if [ "${deb_pkgs}" ] ; then
+	sudo apt-get update
+	sudo apt-get -y install ${deb_pkgs}
+fi
 
