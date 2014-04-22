0a3ad68c67a12fb6136d5cda9a3ac549ebc61b16
Robert Nelson <robertcnelson@gmail.com>
2014-04-22 12:10:17 -0500

    bb.org: blanker, only blank when not blank
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/bone/tester/eeprom-blank.sh
+++ device/bone/tester/eeprom-blank.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -e
 
-eeprom_location=$(ls /sys/devices/ocp.*/44e0b000.i2c/i2c-0/0-0050/eeprom 2> /dev/null)
-if [ ! "x${eeprom_location}" = "x" ] ; then
-	echo "Blanking EEPROM"
-	sudo dd if=/dev/zero of=${eeprom_location} bs=1K count=32
+if ! id | grep -q root; then
+	echo "must be run as root"
+	exit
 fi
 
 eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
+eeprom_location=$(ls /sys/devices/ocp.*/44e0b000.i2c/i2c-0/0-0050/eeprom 2> /dev/null)
+eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -s 5 -n 3)
+if [ "x${eeprom_header}" = "x335" ] ; then
+	if [ ! "x${eeprom_location}" = "x" ] ; then
+		echo "Blanking EEPROM"
+		dd if=/dev/zero of=${eeprom_location} bs=1K count=1
+	fi
+fi
 
 eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -s 5 -n 3)
 if [ "x${eeprom_header}" = "x335" ] ; then
@@ -15,5 +22,5 @@ else
 	echo "EEPROM: blanked"
 fi
 
-sudo dd if=/dev/zero of=/dev/mmcblk1 bs=1M count=16
-sudo touch /boot/uboot/flash-eMMC.txt
+dd if=/dev/zero of=/dev/mmcblk1 bs=1M count=16
+touch /boot/uboot/flash-eMMC.txt
