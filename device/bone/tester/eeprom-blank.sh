e69c6216f3aaecd1bd6e6a1975211d27657d4a22
Robert Nelson <robertcnelson@gmail.com>
2014-04-22 11:46:42 -0500

    bb.org: tester, blank via sudo
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/bone/tester/eeprom-blank.sh
+++ device/bone/tester/eeprom-blank.sh
@@ -3,7 +3,7 @@
 eeprom_location=$(ls /sys/devices/ocp.*/44e0b000.i2c/i2c-0/0-0050/eeprom 2> /dev/null)
 if [ ! "x${eeprom_location}" = "x" ] ; then
 	echo "Blanking EEPROM"
-	dd if=/dev/zero of=${eeprom_location}
+	sudo dd if=/dev/zero of=${eeprom_location}
 fi
 
 eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
