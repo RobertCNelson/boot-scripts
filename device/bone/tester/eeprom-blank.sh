cb65941acc1ad7b17182a94b03ff7dd35184fa9e
Robert Nelson <robertcnelson@gmail.com>
2014-04-22 12:50:46 -0500

    next
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/bone/tester/eeprom-blank.sh
+++ device/bone/tester/eeprom-blank.sh
@@ -22,6 +22,8 @@ else
 	echo "EEPROM: blanked"
 fi
 
+#Might be run twice...
+sed -i -e 's:CAPE=BB-BONE-eMMC1-01:CAPE:g' /etc/default/capemgr
 sed -i -e 's:CAPE:CAPE=BB-BONE-eMMC1-01:g' /etc/default/capemgr
 
 dd if=/dev/zero of=/dev/mmcblk1 bs=1M count=16
