c0706c7850cec5d53045e77e4aca6ad365a9a651
Robert Nelson <robertcnelson@gmail.com>
2014-04-22 11:16:34 -0500

    bb.org: blank eeprom
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/bone/tester/eeprom-blank.sh
+++ device/bone/tester/eeprom-blank.sh
@@ -1,4 +1,19 @@
 #!/bin/bash -e
 
+eeprom_location=$(ls /sys/devices/ocp.*/44e0b000.i2c/i2c-0/0-0050/eeprom 2> /dev/null)
+if [ ! "x${eeprom_location}" = "x" ] ; then
+	echo "Blanking EEPROM"
+	dd if=/dev/zero of=${eeprom_location}
+fi
+
+eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
+
+eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -s 5 -n 3)
+if [ "x${eeprom_header}" = "x335" ] ; then
+	echo "EEPROM: blanking failed"
+else
+	echo "EEPROM: blanked"
+fi
+
 sudo dd if=/dev/zero of=/dev/mmcblk1 bs=1M count=16
 sudo touch /boot/uboot/flash-eMMC.txt
