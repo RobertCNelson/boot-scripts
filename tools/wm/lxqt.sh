2ed043d386c30dcec68f9e0536274695a1f03bd0
Robert Nelson <robertcnelson@gmail.com>
2014-05-08 16:52:40 -0500

    one more
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/wm/lxqt.sh
+++ tools/wm/lxqt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo apt-get update ; sudo apt-get -y install cmake automake gtk-doc-tools libqt4-dev libx11-dev pkg-config libglib2.0-dev libpango1.0-dev valac libmagic-dev
+sudo apt-get update ; sudo apt-get -y install cmake automake gtk-doc-tools libqt4-dev libx11-dev pkg-config libglib2.0-dev libpango1.0-dev valac libmagic-dev libstatgrab-dev
 
 git clone git://git.lxde.org/git/lxde/lxqt.git
 cd lxqt
