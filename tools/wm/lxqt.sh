26b1475eb35fa608426d82ec2b0b1fc3f031e7c5
Robert Nelson <robertcnelson@gmail.com>
2014-06-20 16:24:13 -0500

    next
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/wm/lxqt.sh
+++ tools/wm/lxqt.sh
@@ -5,6 +5,6 @@ sudo apt-get update ; sudo apt-get -y install cmake automake gtk-doc-tools libqt
 git clone git://git.lxde.org/git/lxde/lxqt.git
 cd lxqt
 sed -i -e 's/git@git.lxde.org:/git:\/\/git.lxde.org\/git/g' .gitmodules
-sed -i -e 's/git@gitorious.org:/git:\/\/git.lxde.org\/git/g' .gitmodules
+sed -i -e 's/git@gitorious.org:/git:\/\/git.lxde.org\/git\//g' .gitmodules
 git submodule init
 git submodule update --remote --rebase
