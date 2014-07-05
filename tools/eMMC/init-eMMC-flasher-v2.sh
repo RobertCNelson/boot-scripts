1e1f3458a57c86f567ee3ede25a1f1a98edf1196
Robert Nelson <robertcnelson@gmail.com>
2014-07-04 21:25:57 -0500

    eMMC: none
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/eMMC/init-eMMC-flasher-v2.sh
+++ tools/eMMC/init-eMMC-flasher-v2.sh
@@ -293,10 +293,10 @@ copy_rootfs () {
 	[ -e /proc/$CYLON_PID ]  && kill $CYLON_PID
 
 	if [ -e /sys/class/leds/beaglebone\:green\:usr0/trigger ] ; then
-		echo off > /sys/class/leds/beaglebone\:green\:usr0/trigger
-		echo off > /sys/class/leds/beaglebone\:green\:usr1/trigger
-		echo off > /sys/class/leds/beaglebone\:green\:usr2/trigger
-		echo off > /sys/class/leds/beaglebone\:green\:usr3/trigger
+		echo none > /sys/class/leds/beaglebone\:green\:usr0/trigger
+		echo none > /sys/class/leds/beaglebone\:green\:usr1/trigger
+		echo none > /sys/class/leds/beaglebone\:green\:usr2/trigger
+		echo none > /sys/class/leds/beaglebone\:green\:usr3/trigger
 	fi
 
 	echo ""
