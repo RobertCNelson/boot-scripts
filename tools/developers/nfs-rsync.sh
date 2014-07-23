7bca3c3287e6ff1cbc93f3a259d2036a1ae23f2b
Robert Nelson <robertcnelson@gmail.com>
2014-07-23 11:01:47 -0500

    nfs for me
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/developers/nfs-rsync.sh
+++ tools/developers/nfs-rsync.sh
@@ -5,4 +5,12 @@ echo "sudo mount -t nfs -o rw,nfsvers=3,rsize=8192,wsize=8192 192.168.0.10:/opt/
 
 if [ -d /tmp/rootfs/ ] ; then
 	sudo rsync -aAXv --delete /* /tmp/rootfs/ --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found}
+	sudo sh -c "echo 'debugfs  /sys/kernel/debug  debugfs  defaults  0  0' > /tmp/rootfs/etc/fstab"
+
+	echo "client_ip=`ip addr list eth0 |grep "inet " |cut`"
+	echo "server_ip=192.168.0.10"
+	echo "gw_ip=192.168.0.1"
+	echo "root_dir=/opt/wheezy"
 fi
+
+#
