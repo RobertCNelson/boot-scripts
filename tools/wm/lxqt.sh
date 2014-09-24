1115b8cd4bc8c830af0da10f088a6e521075600b
Robert Nelson <robertcnelson@gmail.com>
2014-09-24 16:27:13 -0500

    a few more
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/wm/lxqt.sh
+++ tools/wm/lxqt.sh
@@ -2,8 +2,6 @@
 
 files () {
 	echo "file list"
-	#compton-conf-qt5 (0.1.0-1) [x]
-	#compton-conf-qt5-dbg (0.1.0-1) [x]
 	#libfm-data (1.2.2.siduction.2) 
 	#libfm-dbg (1.2.2.siduction.2) 
 	#libfm-dev (1.2.2.siduction.2) 
@@ -30,13 +28,6 @@ files () {
 	#liblxqtmount-qt5-0 (0.7.96-1) 
 	#liblxqtmount-qt5-0-dbg (0.7.96-1) 
 	#liblxqtmount-qt5-0-dev (0.7.96-1) 
-	#libpolkit-qt-1-1 (0.112.0-1) 
-	#libpolkit-qt-1-dev (0.112.0-1) 
-	#libpolkit-qt5-1-1 (0.112.0-1) 
-	#libpolkit-qt5-1-dev (0.112.0-1) 
-	#libqt5xdg-dbg (1.0.0-1) [x]
-	#libqt5xdg-dev (1.0.0-1) [x]
-	#libqt5xdg1 (1.0.0-1) [x]
 	#libsysstat-qt5-0 (0.1.0-2) 
 	#libsysstat-qt5-0-dbg (0.1.0-2) 
 	#libsysstat-qt5-0-dev (0.1.0-2) 
@@ -44,7 +35,6 @@ files () {
 	#lxappearance-dbg (0.5.5.siduction.5) 
 	#lximage-qt (0.2.0.siduction.5) 
 	#lximage-qt-dbg (0.2.0.siduction.5) 
-	#lxmenu-data (0.1.4.siduction.1) 
 	#lxqt-about-qt5 (0.7.96-1) 
 	#lxqt-admin-qt5 (0.7.96-1) 
 	#lxqt-admin-qt5-dbg (0.7.96-1) 
@@ -70,8 +60,6 @@ files () {
 	#lxqt-runner-qt5-dbg (0.7.96-1) 
 	#lxqt-session-qt5 (0.7.96-1) 
 	#lxqt-session-qt5-dbg (0.7.96-1) 
-	#obconf-qt5 (0.1.0-1) 
-	#obconf-qt5-dbg (0.1.0-1) 
 	#pcmanfm (1.2.2.siduction.1) 
 	#pcmanfm-dbg (1.2.2.siduction.1) 
 	#pcmanfm-qt5 (0.8.0-2) 
@@ -79,12 +67,25 @@ files () {
 }
 
 build_pkg () {
-	if [ ! -f ${deb_package}_${pkg_version}-${deb_version}_${deb_arch}.deb ] ; then
+	if [ ! -f ${deb_file} ] ; then
 		wget -c ${mirror}/${location}/${package}_${pkg_version}-${deb_version}.dsc
 		wget -c ${mirror}/${location}/${package}_${pkg_version}.orig.tar.${orig_comp}
 		wget -c ${mirror}/${location}/${package}_${pkg_version}-${deb_version}.debian.tar.xz
 
 		dpkg-source -x ${package}_${pkg_version}-${deb_version}.dsc
+		cd ${package}-${pkg_version}/
+		dpkg-buildpackage -rfakeroot -b -uc
+		cd ../
+		ls | grep -v debian | grep .deb
+	fi
+}
+
+build_siduction_pkg () {
+	if [ ! -f ${deb_file} ] ; then
+		wget -c ${mirror}/${location}/${package}_${pkg_version}.dsc
+		wget -c ${mirror}/${location}/${package}_${pkg_version}.tar.xz
+
+		dpkg-source -x ${package}_${pkg_version}.dsc
 		cd ${package}-${pkg_version}/
 		dpkg-buildpackage -rfakeroot -b -uc
 		cd ../
@@ -100,46 +101,61 @@ deb_arch=$(dpkg --print-architecture)
 mirror="http://packages.siduction.org"
 
 package="compton-conf"
-deb_package="compton-conf-qt5"
 location="lxqt/pool/main/c/${package}"
 pkg_version="0.1.0"
 deb_version="1"
+deb_file="compton-conf-qt5_${pkg_version}-${deb_version}_${deb_arch}.deb"
 orig_comp="bz2"
 
 build_pkg
 
 package="polkit-qt-1"
-deb_package="libpolkit-qt-1-dev"
 location="lxqt/pool/main/p/${package}"
 pkg_version="0.112.0"
 deb_version="1"
+deb_file="libpolkit-qt-1-dev_${pkg_version}-${deb_version}_${deb_arch}.deb"
 orig_comp="bz2"
+
 
 build_pkg
 
 package="obconf-qt"
-deb_package="obconf-qt5"
 location="lxqt/pool/main/o/${package}"
 pkg_version="0.1.0"
 deb_version="1"
+deb_file="obconf-qt5_${pkg_version}-${deb_version}_${deb_arch}.deb"
 orig_comp="bz2"
+
 
 build_pkg
 
 package="libqtxdg"
-deb_package="libqt5xdg-dev"
 location="lxqt/pool/main/libq/${package}"
 pkg_version="1.0.0"
 deb_version="1"
+deb_file="libqt5xdg-dev_${pkg_version}-${deb_version}_${deb_arch}.deb"
 orig_comp="gz"
 
 build_pkg
 
+package="lxmenu-data"
+location="lxqt/pool/main/l/${package}"
+pkg_version="0.1.4.siduction.1"
+deb_file="lxmenu-data_${pkg_version}_all.deb"
 
+build_siduction_pkg
 
+package="lxappearance"
+deb_package="lxmenu-data"
+location="lxqt/pool/main/l/${package}"
+pkg_version="0.5.5.siduction.5"
+deb_file="lxappearance_${pkg_version}_${deb_arch}.deb"
+
+build_siduction_pkg
 
 #wip:
 
+#needs: lxmenu-data
 
 package="libfm"
 deb_package="obconf-qt5"
