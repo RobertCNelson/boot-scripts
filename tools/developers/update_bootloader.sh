9e0a3dd76ecc4d2f4f0a51759c070a500447e1ca
Robert Nelson <robertcnelson@gmail.com>
2014-12-02 18:25:30 -0600

    add omap5
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/developers/update_bootloader.sh
+++ tools/developers/update_bootloader.sh
@@ -207,7 +207,7 @@ dd_spl_uboot_boot () {
 
 got_board () {
 	case "${conf_board}" in
-	am335x_evm)
+	am335x_evm|omap5_uevm)
 		is_omap
 		;;
 	esac
@@ -258,6 +258,30 @@ check_soc_sh () {
 
 	if [ -f /boot/SOC.sh ] ; then
 		. /boot/SOC.sh
+		mkdir -p /tmp/uboot/
+		mount /dev/mmcblk0p1 /tmp/uboot/
+		DRIVE="/tmp/uboot"
+		if [ "x${board}" != "x" ] ; then
+
+			if [ "x${board}" = "xam335x_boneblack" ] ; then
+				#Special eeprom less u-boot, switch them to normal on upgrades
+				sed -i -e 's:am335x_boneblack:am335x_evm:g' ${DRIVE}/SOC.sh
+				board="am335x_evm"
+			fi
+
+			conf_board="${board}"
+			got_board
+		else
+			echo "Sorry: board undefined in [${DRIVE}/SOC.sh] can not update bootloader safely"
+			exit
+		fi
+		sync
+		sync
+		umount /tmp/uboot/ || true
+	fi
+
+	if [ -f /boot/uboot/SOC.sh ] ; then
+		. /boot/uboot/SOC.sh
 		mkdir -p /tmp/uboot/
 		mount /dev/mmcblk0p1 /tmp/uboot/
 		DRIVE="/tmp/uboot"
