6a646ccc0df8af863fc884a40d02f32ba4545a34
Robert Nelson <robertcnelson@gmail.com>
2014-12-10 09:06:25 -0600

    am335x: pass a few more things
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- boot/am335x_evm.sh
+++ boot/am335x_evm.sh
@@ -81,7 +81,8 @@ else
 fi
 
 if [ "x${root_drive}" = "x/dev/mmcblk0p1" ] || [ "x${root_drive}" = "x/dev/mmcblk0p1" ] ; then
-	modprobe g_ether host_addr=${cpsw_1_mac}
+	modprobe g_ether iSerialNumber=${SERIAL_NUMBER} iManufacturer=Circuitco iProduct=BeagleBone${BLACK} host_addr=${cpsw_1_mac}
+fi
 else
 	boot_drive="${root_drive%?}1"
 	modprobe g_multi file=${boot_drive} cdrom=0 ro=0 stall=0 removable=1 nofua=1 iSerialNumber=${SERIAL_NUMBER} iManufacturer=Circuitco iProduct=BeagleBone${BLACK} host_addr=${cpsw_1_mac}
