7068eda39297dc971d14a33fc589bf273da5c1db
Robert Nelson <robertcnelson@gmail.com>
2015-01-05 10:51:13 -0600

    beagle_x15: has two ethernet ports
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- boot/beagle_x15.sh
+++ boot/beagle_x15.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 #
-# Copyright (c) 2013-2014 Robert Nelson <robertcnelson@gmail.com>
+# Copyright (c) 2013-2015 Robert Nelson <robertcnelson@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,7 @@
 # THE SOFTWARE.
 
 eth0_addr=$(ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
+eth1_addr=$(ip addr list eth1 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
 usb0_addr=$(ip addr list usb0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
 wlan0_addr=$(ip addr list wlan0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
 
@@ -28,6 +29,9 @@ sed -i -e '/Address/d' /etc/issue
 
 if [ ! "x${eth0_addr}" = "x" ] ; then
 	echo "The IP Address for eth0 is: ${eth0_addr}" >> /etc/issue
+fi
+if [ ! "x${eth1_addr}" = "x" ] ; then
+	echo "The IP Address for eth1 is: ${eth1_addr}" >> /etc/issue
 fi
 if [ ! "x${wlan0_addr}" = "x" ] ; then
 	echo "The IP Address for wlan0 is: ${wlan0_addr}" >> /etc/issue
