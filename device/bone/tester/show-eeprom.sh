b9fdec03ed8bb33b131fd0718bf9562406a62027
Robert Nelson <robertcnelson@gmail.com>
2015-02-19 10:04:50 -0600

    show: fixed
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/bone/tester/show-eeprom.sh
+++ device/bone/tester/show-eeprom.sh
@@ -6,5 +6,5 @@ if ! id | grep -q root; then
 fi
 
 eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
-eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -n 16))
+eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -n 16)
 echo "eeprom: [${eeprom_header}]"
