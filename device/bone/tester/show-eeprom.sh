8ca53a4e1e4b5cdd7a1d348afdccb2958bcb775c
Robert Nelson <robertcnelson@gmail.com>
2015-02-19 10:11:54 -0600

    add raw dump
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/bone/tester/show-eeprom.sh
+++ device/bone/tester/show-eeprom.sh
@@ -7,4 +7,6 @@ fi
 
 eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
 eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -n 16)
+eeprom_raw=$(hexdump ${eeprom} -n 8 | grep -v 0000008)
 echo "eeprom: [${eeprom_header}]"
+echo "eeprom raw: [${eeprom_raw}]"
