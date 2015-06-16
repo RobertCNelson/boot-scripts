f63802f9eddd05339750468b194e792813869e41
Robert Nelson <robertcnelson@gmail.com>
2015-06-16 08:57:53 -0500

    autoconfigure_usb0: use /tmp for udhcpd.conf storage on ro systems
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- boot/autoconfigure_usb0.sh
+++ boot/autoconfigure_usb0.sh
@@ -46,8 +46,9 @@ unset deb_etc_dir \
 # Separated /etc variable to ease test on host system (by changing
 # etc_dir during test).
 deb_etc_dir=/etc
+deb_ro_dir=$(mktemp -d)
 
-deb_udhcpd_conf=${deb_etc_dir}/udhcpd.conf
+deb_udhcpd_conf=${deb_ro_dir}/udhcpd.conf
 deb_udhcpd_default=${deb_etc_dir}/default/udhcpd
 
 deb_dnsmasq_conf=${deb_etc_dir}/dnsmasq.conf
