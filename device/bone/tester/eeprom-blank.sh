092c1534a6615e73b27875d1b8ef4be3039cad96
Robert Nelson <robertcnelson@gmail.com>
2015-06-18 10:06:36 -0500

    eeprom-blank.sh: update for v4.1.x
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/bone/tester/eeprom-blank.sh
+++ device/bone/tester/eeprom-blank.sh
@@ -5,9 +5,22 @@ if ! id | grep -q root; then
 	exit
 fi
 
-eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
-eeprom_location=$(ls /sys/devices/ocp.*/44e0b000.i2c/i2c-0/0-0050/eeprom 2> /dev/null)
-eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -s 5 -n 3)
+if [ -f /sys/class/nvmem/at24-0/nvmem ] ; then
+	message="4.1.x+ kernel with nvmem detected..." ; broadcast
+	eeprom="/sys/class/nvmem/at24-0/nvmem"
+
+	#with 4.1.x: -s 5 isn't working...
+	#eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -s 5 -n 3) = blank...
+	#hexdump -e '8/1 "%c"' ${eeprom} -n 8 = �U3�A335
+	eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -n 8 | cut -b 6-8)
+
+	eeprom_location="/sys/devices/platform/ocp/44e0b000.i2c/i2c-0/0-0050/nvmem/at24-0/nvmem"
+else
+	eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
+	eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -s 5 -n 3)
+	eeprom_location=$(ls /sys/devices/ocp*/44e0b000.i2c/i2c-0/0-0050/eeprom 2> /dev/null)
+fi
+
 if [ "x${eeprom_header}" = "x335" ] ; then
 	if [ ! "x${eeprom_location}" = "x" ] ; then
 		echo "Blanking EEPROM"
@@ -15,7 +28,7 @@ if [ "x${eeprom_header}" = "x335" ] ; then
 	fi
 fi
 
-eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -s 5 -n 3)
+eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -n 8 | cut -b 6-8)
 if [ "x${eeprom_header}" = "x335" ] ; then
 	echo "EEPROM: blanking failed"
 else
