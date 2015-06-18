ceda7859a6cae863749e8741a32645cb53b85fe3
Robert Nelson <robertcnelson@gmail.com>
2015-06-18 10:01:24 -0500

    tester: update show-eeprom.sh
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/bone/tester/show-eeprom.sh
+++ device/bone/tester/show-eeprom.sh
@@ -5,7 +5,14 @@ if ! id | grep -q root; then
 	exit
 fi
 
-eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
+if [ -f /sys/bus/i2c/devices/0-0050/eeprom ] ; then
+	eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
+fi
+
+if [ -f /sys/class/nvmem/at24-0/nvmem ] ; then
+	eeprom="/sys/class/nvmem/at24-0/nvmem"
+fi
+
 eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -n 16)
 eeprom_raw=$(hexdump ${eeprom} -n 8 | grep -v 0000008)
 echo "eeprom: [${eeprom_header}]"
