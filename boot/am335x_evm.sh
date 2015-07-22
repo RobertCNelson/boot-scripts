2d8032b0f4cc6546e66e468f995f65bc6b40d641
Robert Nelson <robertcnelson@gmail.com>
2015-07-22 15:21:04 -0500

    am335x_evm: am335x-boneblack-emmc-overlay.dtb load univ-emmc
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- boot/am335x_evm.sh
+++ boot/am335x_evm.sh
@@ -251,10 +251,7 @@ if [ -f /sys/devices/platform/bone_capemgr/slots ] ; then
 				#config-pin overlay cape-univ-emmc
 				;;
 			am335x-boneblack-emmc-overlay.dtb)
-				config-pin overlay cape-universal
-				#config-pin overlay cape-univ-hdmi
-				#config-pin overlay cape-univ-audio
-				##config-pin overlay cape-univ-emmc
+				config-pin overlay univ-emmc
 				;;
 			am335x-boneblack-hdmi-overlay.dtb)
 				config-pin overlay cape-universal
