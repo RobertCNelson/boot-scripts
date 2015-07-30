cdfcd9b8600c39e4291c4f61269229afcd4ce209
Robert Nelson <robertcnelson@gmail.com>
2015-07-30 09:24:24 -0500

    x15: add iperf call
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/x15/test_dual_eth.sh
+++ device/x15/test_dual_eth.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+#based on: http://www.embeddedsystemtesting.com/2015/03/how-to-run-iperf-traffic-on-same.html?m=1
+
+#needs: iperf
+
 if ! id | grep -q root; then
 	echo "must be run as root"
 	exit
@@ -10,13 +14,22 @@ mac_eth1=$(ifconfig -a | grep eth1 | awk '{print $5}')
 
 ifconfig eth0 10.50.0.1 netmask 255.255.255.0
 ifconfig eth1 10.50.1.1 netmask 255.255.255.0
+
 iptables -t nat -L
 iptables -t nat -A POSTROUTING -s 10.50.0.1 -d 10.60.1.1 -j SNAT --to-source 10.60.0.1
 iptables -t nat -A PREROUTING -d 10.60.0.1 -j DNAT --to-destination 10.50.0.1
 iptables -t nat -A POSTROUTING -s 10.50.1.1 -d 10.60.0.1 -j SNAT --to-source 10.60.1.1
 iptables -t nat -A PREROUTING -d 10.60.1.1 -j DNAT --to-destination 10.50.1.1
+
 ip route add 10.60.1.1 dev eth0
 arp -i eth0 -s 10.60.1.1 ${mac_eth1}
 ip route add 10.60.0.1 dev eth1
 arp -i eth1 -s 10.60.0.1 ${mac_eth0}
+
 ping -c 1 10.60.1.1
+
+# server
+iperf -B 10.50.0.1 -s -u -w 256k -l 1KB &
+# client
+iperf -B 10.50.1.1 -c 10.60.0.1 -u -b 600M -w 256k -l 1KB -P 10 -t 60
+
