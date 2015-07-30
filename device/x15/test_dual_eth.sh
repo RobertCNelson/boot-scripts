8d5da74db78da0a3a2c21a76670306999129786f
Robert Nelson <robertcnelson@gmail.com>
2015-07-30 09:20:58 -0500

    add test for dual_eth
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ device/x15/test_dual_eth.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+if ! id | grep -q root; then
+	echo "must be run as root"
+	exit
+fi
+
+mac_eth0=$(ifconfig -a | grep eth0 | awk '{print $5}')
+mac_eth1=$(ifconfig -a | grep eth1 | awk '{print $5}')
+
+ifconfig eth0 10.50.0.1 netmask 255.255.255.0
+ifconfig eth1 10.50.1.1 netmask 255.255.255.0
+iptables -t nat -L
+iptables -t nat -A POSTROUTING -s 10.50.0.1 -d 10.60.1.1 -j SNAT --to-source 10.60.0.1
+iptables -t nat -A PREROUTING -d 10.60.0.1 -j DNAT --to-destination 10.50.0.1
+iptables -t nat -A POSTROUTING -s 10.50.1.1 -d 10.60.0.1 -j SNAT --to-source 10.60.1.1
+iptables -t nat -A PREROUTING -d 10.60.1.1 -j DNAT --to-destination 10.50.1.1
+ip route add 10.60.1.1 dev eth0
+arp -i eth0 -s 10.60.1.1 ${mac_eth1}
+ip route add 10.60.0.1 dev eth1
+arp -i eth1 -s 10.60.0.1 ${mac_eth0}
+ping -c 1 10.60.1.1
