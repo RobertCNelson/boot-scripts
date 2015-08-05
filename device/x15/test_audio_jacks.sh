8ced61d833e387c09864f1a06cf9e991f577abdf
Robert Nelson <robertcnelson@gmail.com>
2015-08-05 12:15:10 -0500

    audio: too long
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/x15/test_audio_jacks.sh
+++ device/x15/test_audio_jacks.sh
@@ -11,10 +11,11 @@ amixer -c0 sset 'PCM' 119
 amixer -c0 sset 'Line DAC' 108
 amixer -c0 sset 'Left PGA Mixer Mic2L' unmute
 amixer -c0 sset 'Right PGA Mixer Mic2R' unmute
-amixer -c0 sset 'PGA' 10
+#amixer -c0 sset 'PGA' 10
+amixer -c0 sset 'PGA' 30
 
-echo "#Recording: 10 seconds"
-arecord -Dplughw:0,0 -fcd -d10 -c2 > /tmp/music.wav
+echo "#Recording: 4 seconds"
+arecord -Dplughw:0,0 -fcd -d4 -c2 > /tmp/music.wav
 
 echo "#Playback:"
 aplay -Dplughw:0,0 /tmp/music.wav
