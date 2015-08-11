286e86e917f1a9d659b0c1dd6b2527b3f186af5f
Robert Nelson <robertcnelson@gmail.com>
2015-08-11 10:45:49 -0500

    update eeprom check
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/bone/tester/show-eeprom.sh
+++ device/bone/tester/show-eeprom.sh
@@ -5,15 +5,29 @@ if ! id | grep -q root; then
 	exit
 fi
 
-if [ -f /sys/bus/i2c/devices/0-0050/eeprom ] ; then
-	eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
+unset got_eeprom
+
+#v8 of nvmem...
+if [ -f /sys/bus/nvmem/devices/at24-0/nvmem ] && [ "x${got_eeprom}" = "x" ] ; then
+	eeprom="/sys/bus/nvmem/devices/at24-0/nvmem"
+	got_eeprom="true"
 fi
 
-if [ -f /sys/class/nvmem/at24-0/nvmem ] ; then
+#pre-v8 of nvmem...
+if [ -f /sys/class/nvmem/at24-0/nvmem ] && [ "x${got_eeprom}" = "x" ] ; then
 	eeprom="/sys/class/nvmem/at24-0/nvmem"
+	got_eeprom="true"
 fi
 
-eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -n 16)
-eeprom_raw=$(hexdump ${eeprom} -n 8 | grep -v 0000008)
-echo "eeprom: [${eeprom_header}]"
-echo "eeprom raw: [${eeprom_raw}]"
+#eeprom...
+if [ -f /sys/bus/i2c/devices/0-0050/eeprom ] && [ "x${got_eeprom}" = "x" ] ; then
+	eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
+	got_eeprom="true"
+fi
+
+if [ "x${got_eeprom}" = "xtrue" ] ; then
+	eeprom_header=$(hexdump -e '8/1 "%c"' ${eeprom} -n 16)
+	eeprom_raw=$(hexdump ${eeprom} -n 8 | grep -v 0000008)
+	echo "eeprom: [${eeprom_header}]"
+	echo "eeprom raw: [${eeprom_raw}]"
+fi
