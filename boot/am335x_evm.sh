dd45eb1ef80a226d73933c4c42f6d3e7154fe9fb
Robert Nelson <robertcnelson@gmail.com>
2015-08-18 12:47:28 -0500

    am335x: disable wlan reporting
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- boot/am335x_evm.sh
+++ boot/am335x_evm.sh
@@ -163,16 +163,16 @@ $(dirname $0)/autoconfigure_usb0.sh
 
 eth0_addr=$(ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1 2>/dev/null || true)
 usb0_addr=$(ip addr list usb0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1 2>/dev/null || true)
-wlan0_addr=$(ip addr list wlan0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1 2>/dev/null || true)
+#wlan0_addr=$(ip addr list wlan0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1 2>/dev/null || true)
 
 sed -i -e '/Address/d' /etc/issue
 
 if [ ! "x${eth0_addr}" = "x" ] ; then
 	echo "The IP Address for eth0 is: ${eth0_addr}" >> /etc/issue
 fi
-if [ ! "x${wlan0_addr}" = "x" ] ; then
-	echo "The IP Address for wlan0 is: ${wlan0_addr}" >> /etc/issue
-fi
+#if [ ! "x${wlan0_addr}" = "x" ] ; then
+#	echo "The IP Address for wlan0 is: ${wlan0_addr}" >> /etc/issue
+#fi
 if [ ! "x${usb0_addr}" = "x" ] ; then
 	echo "The IP Address for usb0 is: ${usb0_addr}" >> /etc/issue
 fi
