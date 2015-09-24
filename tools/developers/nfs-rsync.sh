417451996ba8fab08c7929641f7196df92b416b7
Robert Nelson <robertcnelson@gmail.com>
2015-09-24 16:18:53 -0500

    nfs: workaround connman
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/developers/nfs-rsync.sh
+++ tools/developers/nfs-rsync.sh
@@ -13,6 +13,18 @@ if [ -d /tmp/rootfs/ ] ; then
 	sudo rsync -aAXv --delete /* /tmp/rootfs/ --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found}
 	sudo sh -c "echo 'debugfs  /sys/kernel/debug  debugfs  defaults  0  0' > /tmp/rootfs/etc/fstab"
 
+	if [ -f /tmp/rootfs/lib/systemd/system/connman.service ] ; then
+		unset check_connman
+		check_connman=$(cat /tmp/rootfs/lib/systemd/system/connman.service | grep ExecStart)
+		if [ ! "x${check_connman}" = "x" ] ; then
+			unset check_connman
+			check_connman=$(cat /tmp/rootfs/lib/systemd/system/connman.service | grep ExecStart | grep eth0)
+			if [ "x${check_connman}" = "x" ] ; then
+				sudo sed -i -e 's:-n:-n -I eth0:g' /tmp/rootfs/lib/systemd/system/connman.service
+			fi
+		fi
+	fi
+
 	echo "client_ip=`ip addr list eth0 | grep \"inet \" |cut -d' ' -f6|cut -d/ -f1`"
 	echo "server_ip=192.168.0.10"
 	echo "gw_ip=192.168.0.1"
