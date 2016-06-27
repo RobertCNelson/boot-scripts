b574b13fc2f87ccd7ef304af510ba3cdd4f07409
Baozhu <zuobaozhu@gmail.com>
2016-06-27 07:03:52 +0000

    finish tools/eMMC/init-eMMC-flasher-from-usb-media-v1-bbgw.sh

--- tools/eMMC/init-eMMC-flasher-from-usb-media-v1-bbgw.sh
+++ tools/eMMC/init-eMMC-flasher-from-usb-media-v1-bbgw.sh
@@ -139,7 +139,160 @@ print_eeprom () {
 		message="-----------------------------" ; broadcast
 	fi
 }
+dd_bootloader () {
+	message="Writing bootloader to [${destination}]" ; broadcast
 
+	unset dd_spl_uboot
+	if [ ! "x${dd_spl_uboot_count}" = "x" ] ; then
+		dd_spl_uboot="${dd_spl_uboot}count=${dd_spl_uboot_count} "
+	fi
+
+	if [ ! "x${dd_spl_uboot_seek}" = "x" ] ; then
+		dd_spl_uboot="${dd_spl_uboot}seek=${dd_spl_uboot_seek} "
+	fi
+
+	if [ ! "x${dd_spl_uboot_conf}" = "x" ] ; then
+		dd_spl_uboot="${dd_spl_uboot}conv=${dd_spl_uboot_conf} "
+	fi
+
+	if [ ! "x${dd_spl_uboot_bs}" = "x" ] ; then
+		dd_spl_uboot="${dd_spl_uboot}bs=${dd_spl_uboot_bs}"
+	fi
+
+	unset dd_uboot
+	if [ ! "x${dd_uboot_count}" = "x" ] ; then
+		dd_uboot="${dd_uboot}count=${dd_uboot_count} "
+	fi
+
+	if [ ! "x${dd_uboot_seek}" = "x" ] ; then
+		dd_uboot="${dd_uboot}seek=${dd_uboot_seek} "
+	fi
+
+	if [ ! "x${dd_uboot_conf}" = "x" ] ; then
+		dd_uboot="${dd_uboot}conv=${dd_uboot_conf} "
+	fi
+
+	if [ ! "x${dd_uboot_bs}" = "x" ] ; then
+		dd_uboot="${dd_uboot}bs=${dd_uboot_bs}"
+	fi
+
+	message="dd if=${dd_spl_uboot_backup} of=${destination} ${dd_spl_uboot}" ; broadcast
+	echo "-----------------------------"
+	dd if=${dd_spl_uboot_backup} of=${destination} ${dd_spl_uboot}
+	echo "-----------------------------"
+	message="dd if=${dd_uboot_backup} of=${destination} ${dd_uboot}" ; broadcast
+	echo "-----------------------------"
+	dd if=${dd_uboot_backup} of=${destination} ${dd_uboot}
+	message="-----------------------------" ; broadcast
+}
+format_boot () {
+	message="mkfs.vfat -F 16 ${destination}p1 -n ${boot_label}" ; broadcast
+	echo "-----------------------------"
+	mkfs.vfat -F 16 ${destination}p1 -n ${boot_label}
+	echo "-----------------------------"
+	flush_cache
+}
+
+format_root () {
+	message="mkfs.ext4 ${destination}p2 -L ${rootfs_label}" ; broadcast
+	echo "-----------------------------"
+	mkfs.ext4 ${destination}p2 -L ${rootfs_label}
+	echo "-----------------------------"
+	flush_cache
+}
+
+format_single_root () {
+	message="mkfs.ext4 ${destination}p1 -L ${boot_label}" ; broadcast
+	echo "-----------------------------"
+	mkfs.ext4 ${destination}p1 -L ${boot_label}
+	echo "-----------------------------"
+	flush_cache
+}
+partition_drive () {
+	if [ -f /boot/SOC.sh ] ; then
+		. /boot/SOC.sh
+	fi
+
+	dd_bootloader
+
+	if [ "x${boot_fstype}" = "xfat" ] ; then
+		conf_boot_startmb=${conf_boot_startmb:-"1"}
+		conf_boot_endmb=${conf_boot_endmb:-"96"}
+		sfdisk_fstype=${sfdisk_fstype:-"0xE"}
+		boot_label=${boot_label:-"BEAGLEBONE"}
+		rootfs_label=${rootfs_label:-"rootfs"}
+
+		message="Formatting: ${destination}" ; broadcast
+
+		sfdisk_options="--force --Linux --in-order --unit M"
+		sfdisk_boot_startmb="${conf_boot_startmb}"
+		sfdisk_boot_endmb="${conf_boot_endmb}"
+
+		test_sfdisk=$(LC_ALL=C sfdisk --help | grep -m 1 -e "--in-order" || true)
+		if [ "x${test_sfdisk}" = "x" ] ; then
+			message="sfdisk: [2.26.x or greater]" ; broadcast
+			sfdisk_options="--force"
+			sfdisk_boot_startmb="${sfdisk_boot_startmb}M"
+			sfdisk_boot_endmb="${sfdisk_boot_endmb}M"
+		fi
+
+		message="sfdisk: [sfdisk ${sfdisk_options} ${destination}]" ; broadcast
+		message="sfdisk: [${sfdisk_boot_startmb},${sfdisk_boot_endmb},${sfdisk_fstype},*]" ; broadcast
+		message="sfdisk: [,,,-]" ; broadcast
+
+		LC_ALL=C sfdisk ${sfdisk_options} "${destination}" <<-__EOF__
+			${sfdisk_boot_startmb},${sfdisk_boot_endmb},${sfdisk_fstype},*
+			,,,-
+		__EOF__
+
+		flush_cache
+		format_boot
+		format_root
+		message="Formatting: ${destination} complete" ; broadcast
+		message="-----------------------------" ; broadcast
+
+		copy_boot
+		media_rootfs="2"
+	else
+		conf_boot_startmb=${conf_boot_startmb:-"1"}
+		sfdisk_fstype=${sfdisk_fstype:-"L"}
+		if [ "x${sfdisk_fstype}" = "x0x83" ] ; then
+			sfdisk_fstype="L"
+		fi
+		boot_label=${boot_label:-"BEAGLEBONE"}
+
+		message="Formatting: ${destination}" ; broadcast
+
+		sfdisk_options="--force --Linux --in-order --unit M"
+		sfdisk_boot_startmb="${conf_boot_startmb}"
+
+		test_sfdisk=$(LC_ALL=C sfdisk --help | grep -m 1 -e "--in-order" || true)
+		if [ "x${test_sfdisk}" = "x" ] ; then
+			message="sfdisk: [2.26.x or greater]" ; broadcast
+			if [ "x${bootrom_gpt}" = "xenable" ] ; then
+				sfdisk_options="--force --label gpt"
+			else
+				sfdisk_options="--force"
+			fi
+			sfdisk_boot_startmb="${sfdisk_boot_startmb}M"
+		fi
+
+		message="sfdisk: [$(LC_ALL=C sfdisk --version)]" ; broadcast
+		message="sfdisk: [sfdisk ${sfdisk_options} ${destination}]" ; broadcast
+		message="sfdisk: [${sfdisk_boot_startmb},${sfdisk_boot_endmb},${sfdisk_fstype},*]" ; broadcast
+
+		LC_ALL=C sfdisk ${sfdisk_options} "${destination}" <<-__EOF__
+			${sfdisk_boot_startmb},,${sfdisk_fstype},*
+		__EOF__
+
+		flush_cache
+		format_single_root
+		message="Formatting: ${destination} complete" ; broadcast
+		message="-----------------------------" ; broadcast
+
+		media_rootfs="1"
+	fi
+}
 flash_emmc () {
 	message="eMMC: prepareing ${destination}" ; broadcast
 	flush_cache
@@ -155,34 +308,128 @@ flash_emmc () {
 
 	sync
 	flush_cache
+    partition_drive
+    message="-----------------------------" ; broadcast
+	message="Copying: ${conf_image} -> ${destination}p${media_rootfs}" ; broadcast
+    message="mkdir /tmp/dest_rootfs" ; broadcast
+    if [ ! -d /tmp/dest_rootfs ] ; then
+       mkdir /tmp/dest_rootfs || true
+    fi
+	mount ${destination}p${media_rootfs} /tmp/dest_rootfs/ -o async,noatime
 
-	message="mkfs.ext2 -L rootfs ${destination}p1" ; broadcast
-	LC_ALL=C mkfs.ext2 -L rootfs ${destination}p1 || write_failure
-	message="Erasing: ${destination} complete" ; broadcast
+    message="-----------------------------" ; broadcast 
+    message="mount -v -o offset=1048576 -t ext4 /opt/emmc/${conf_image} /tmp/source_rootfs" ; broadcast    
+    if [ ! -d /tmp/source_rootfs ] ; then
+        mkdir /tmp/source_rootfs || true
+    fi 
+    modprobe loop
+    lsmod
+    mount -v -o async,noatime,offset=1048576 -t ext4 /opt/emmc/${conf_image} /tmp/source_rootfs
+
+    df -h
+	message="rsync: /tmp/source_rootfs -> /tmp/dest_rootfs/" ; broadcast
+	rsync -aAx /tmp/source_rootfs/* /tmp/dest_rootfs/ --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/lib/modules/*,/uEnv.txt} || write_failure
+
+	if [ -d /tmp/dest_rootfs/etc/ssh/ ] ; then
+		#ssh keys will now get regenerated on the next bootup
+		touch /tmp/dest_rootfs/etc/ssh/ssh.regenerate
+		flush_cache
+	fi	
+    
+    mkdir -p /tmp/dest_rootfs/lib/modules/$(uname -r)/ || true
+	message="Copying: Kernel modules" ; broadcast
+	message="rsync: /tmp/source_rootfs/lib/modules/$(uname -r)/ -> /tmp/dest_rootfs/lib/modules/$(uname -r)/" ; broadcast
+	rsync -aAx /tmp/source_rootfs/lib/modules/$(uname -r)/* /tmp/dest_rootfs/lib/modules/$(uname -r)/ || write_failure
+	flush_cache
+    
+	message="Final System Tweaks:" ; broadcast
+	unset root_uuid
+	root_uuid=$(/sbin/blkid -c /dev/null -s UUID -o value ${destination}p${media_rootfs})
+	if [ "${root_uuid}" ] ; then
+		sed -i -e 's:uuid=:#uuid=:g' /tmp/dest_rootfs/boot/uEnv.txt
+		echo "uuid=${root_uuid}" >> /tmp/dest_rootfs/boot/uEnv.txt
+
+		message="UUID=${root_uuid}" ; broadcast
+		root_uuid="UUID=${root_uuid}"
+	else
+		#really a failure...
+		root_uuid="${source}p${media_rootfs}"
+	fi  
+
+	message="Generating: /etc/fstab" ; broadcast
+	echo "# /etc/fstab: static file system information." > /tmp/dest_rootfs/etc/fstab
+	echo "#" >> /tmp/dest_rootfs/etc/fstab
+	echo "${root_uuid}  /  ext4  noatime,errors=remount-ro  0  1" >> /tmp/dest_rootfs/etc/fstab
+	echo "debugfs  /sys/kernel/debug  debugfs  defaults  0  0" >> /tmp/dest_rootfs/etc/fstab
+	cat /tmp/dest_rootfs/etc/fstab
+
+	message="/boot/uEnv.txt: disabling eMMC flasher script" ; broadcast
+	script="cmdline=init=/opt/scripts/tools/eMMC/init-eMMC-flasher-v3-bbgw.sh"
+	sed -i -e 's:'$script':#'$script':g' /tmp/dest_rootfs/boot/uEnv.txt
+	cat /tmp/dest_rootfs/boot/uEnv.txt
+	message="-----------------------------" ; broadcast   
+
+	flush_cache
+	message="running: chroot /tmp/dest_rootfs/ /usr/bin/bb-wl18xx-wlan0" ; broadcast
+
+	mount --bind /proc /tmp/dest_rootfs/proc
+	mount --bind /sys /tmp/dest_rootfs/sys
+	mount --bind /dev /tmp/dest_rootfs/dev
+	mount --bind /dev/pts /tmp/dest_rootfs/dev/pts
+
+	modprobe wl18xx
+	message="-----------------------------" ; broadcast
+	message="lsmod" ; broadcast
+	message="`lsmod`" ; broadcast
+	message="-----------------------------" ; broadcast
+	chroot /tmp/dest_rootfs/ /usr/bin/bb-wl18xx-wlan0
 	message="-----------------------------" ; broadcast
 
-	if [ ! "x${conf_bmap}" = "x" ] ; then
-		if [ -f /usr/bin/bmaptool ] && [ -f ${wdir}/${conf_bmap} ] ; then
-			message="Flashing eMMC with bmaptool" ; broadcast
-			message="-----------------------------" ; broadcast
-			message="bmaptool copy --bmap ${wdir}/${conf_bmap} ${wdir}/${conf_image} ${destination}" ; broadcast
-			/usr/bin/bmaptool copy --bmap ${wdir}/${conf_bmap} ${wdir}/${conf_image} ${destination} || write_failure
-			message="-----------------------------" ; broadcast
-		else
-			message="Flashing eMMC with dd" ; broadcast
-			message="-----------------------------" ; broadcast
-			message="xzcat ${wdir}/${conf_image} | dd of=${destination} bs=1M" ; broadcast
-			xzcat ${wdir}/${conf_image} | dd of=${destination} bs=1M || write_failure
-			message="-----------------------------" ; broadcast
-		fi
-	else
-		message="Flashing eMMC with dd" ; broadcast
-		message="-----------------------------" ; broadcast
-		message="xzcat ${wdir}/${conf_image} | dd of=${destination} bs=1M" ; broadcast
-		xzcat ${wdir}/${conf_image} | dd of=${destination} bs=1M || write_failure
-		message="-----------------------------" ; broadcast
-	fi
 	flush_cache
+	message="initrd: `ls -lh /tmp/dest_rootfs/boot/initrd.img*`" ; broadcast
+
+	umount -fl /tmp/dest_rootfs/dev/pts
+	umount -fl /tmp/dest_rootfs/dev
+	umount -fl /tmp/dest_rootfs/proc
+	umount -fl /tmp/dest_rootfs/sys
+	sleep 2
+
+	flush_cache
+	message="-----------------------------" ; broadcast
+	umount /tmp/dest_rootfs/ || umount -l /tmp/dest_rootfs/ || write_failure  
+    flush_cache    
+    umount /tmp/source_rootfs/ || umount -l /tmp/source_rootfs/ || write_failure  
+    flush_cache    
+	message="Copying: ${conf_image} -> ${destination}p${media_rootfs} complete" ; broadcast
+	message="-----------------------------" ; broadcast    
+    if [ "x${is_bbb}" = "xenable" ] ; then
+		[ -e /proc/$CYLON_PID ]  && kill $CYLON_PID
+	fi
+    
+	if [ -f /boot/debug.txt ] ; then
+		message="This script has now completed its task" ; broadcast
+		message="-----------------------------" ; broadcast
+		message="debug: enabled" ; broadcast
+		inf_loop
+	else
+		umount /tmp || umount -l /tmp
+		if [ "x${is_bbb}" = "xenable" ] ; then
+			if [ -e /sys/class/leds/beaglebone\:green\:usr0/trigger ] ; then
+				echo default-on > /sys/class/leds/beaglebone\:green\:usr0/trigger
+				echo default-on > /sys/class/leds/beaglebone\:green\:usr1/trigger
+				echo default-on > /sys/class/leds/beaglebone\:green\:usr2/trigger
+				echo default-on > /sys/class/leds/beaglebone\:green\:usr3/trigger
+			fi
+		fi
+		mount
+
+		message="eMMC has been flashed: please wait for device to power down." ; broadcast
+		message="-----------------------------" ; broadcast
+
+		flush_cache
+		#To properly shudown, /opt/scripts/boot/am335x_evm.sh is going to call halt:
+		exec /sbin/init
+	fi
 }
 
 etc_mtab_symlink () {
