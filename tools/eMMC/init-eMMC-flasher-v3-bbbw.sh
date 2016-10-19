2de8925731cd287b0818365b6d5498df2475fdcd
Sylvain Lamontagne <sylvain.lamontagne@gmail.com>
2016-10-19 00:36:23 -0400

    Rework library and adapt some init-eMMC scripts

--- tools/eMMC/init-eMMC-flasher-v3-bbbw.sh
+++ tools/eMMC/init-eMMC-flasher-v3-bbbw.sh
@@ -29,98 +29,78 @@ source $(dirname "$0")/functions.sh
 device_eeprom="bbbw-eeprom"
 
 check_if_run_as_root
-find_root_drive
 
-boot_drive="${root_drive%?}1"
+startup_message
+prepare_environment
 
-if [ ! "x${boot_drive}" = "x${root_drive}" ] ; then
-	mount ${boot_drive} /boot/uboot -o ro
-fi
-mount -t tmpfs tmpfs /tmp
-
-if [ "x${boot_drive}" = "x/dev/mmcblk0p1" ] ; then
-	source="/dev/mmcblk0"
-	destination="/dev/mmcblk1"
-fi
-
-if [ "x${boot_drive}" = "x/dev/mmcblk1p1" ] ; then
-	source="/dev/mmcblk1"
-	destination="/dev/mmcblk0"
-fi
+countdown 5
+check_eeprom
+check_running_system
+activate_cylon_leds
 
 #We override the copy_rootfs that comes from functions.sh as this one has wireless added to it
-copy_rootfs () {
-	message="Copying: ${source}p${media_rootfs} -> ${destination}p${media_rootfs}" ; broadcast
-	mkdir -p /tmp/rootfs/ || true
+_copy_rootfs() {
+  empty_line
+  generate_line 80 '='
+  echo_broadcast "Copying: Current rootfs to ${rootfs_partition}"
+  generate_line 40
+  echo_broadcast "==> rsync: / -> ${tmp_rootfs_dir}"
+  generate_line 40
+  rsync -aAx --human-readable --info=name0,progress2 /* ${tmp_rootfs_dir} --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/lib/modules/*,/uEnv.txt} || write_failure
+  flush_cache
+  generate_line 40
+  echo_broadcast "==> Copying: Kernel modules"
+  echo_broadcast "===> Creating directory for modules"
+  mkdir -p ${tmp_rootfs_dir}/lib/modules/$(uname -r)/ || true
+  echo_broadcast "===> rsync: /lib/modules/$(uname -r)/ -> ${tmp_rootfs_dir}/lib/modules/$(uname -r)/"
+  generate_line 40
+  rsync -aAx --human-readable --info=name0,progress2 /lib/modules/$(uname -r)/* ${tmp_rootfs_dir}/lib/modules/$(uname -r)/ || write_failure
+  flush_cache
+  generate_line 40
 
-	mount ${destination}p${media_rootfs} /tmp/rootfs/ -o async,noatime
+  echo_broadcast "Copying: Current rootfs to ${rootfs_partition} complete"
+  generate_line 80 '='
+  empty_line
+  generate_line 80 '='
+  echo_broadcast "Final System Tweaks:"
+  generate_line 40
+  if [ -d ${tmp_rootfs_dir}/etc/ssh/ ] ; then
+    echo_broadcast "==> Applying SSH Key Regeneration trick"
+    #ssh keys will now get regenerated on the next bootup
+    touch ${tmp_rootfs_dir}/etc/ssh/ssh.regenerate
+    flush_cache
+  fi
 
-	message="rsync: / -> /tmp/rootfs/" ; broadcast
-	rsync -aAx /* /tmp/rootfs/ --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/lib/modules/*,/uEnv.txt} || write_failure
-	flush_cache
+  _generate_uEnv ${tmp_rootfs_dir}/boot/uEnv.txt
 
-	if [ -d /tmp/rootfs/etc/ssh/ ] ; then
-		#ssh keys will now get regenerated on the next bootup
-		touch /tmp/rootfs/etc/ssh/ssh.regenerate
-		flush_cache
-	fi
+  _generate_fstab
 
-	mkdir -p /tmp/rootfs/lib/modules/$(uname -r)/ || true
-
-	message="Copying: Kernel modules" ; broadcast
-	message="rsync: /lib/modules/$(uname -r)/ -> /tmp/rootfs/lib/modules/$(uname -r)/" ; broadcast
-	rsync -aAx /lib/modules/$(uname -r)/* /tmp/rootfs/lib/modules/$(uname -r)/ || write_failure
-	flush_cache
-
-	message="Copying: ${source}p${media_rootfs} -> ${destination}p${media_rootfs} complete" ; broadcast
-	message="-----------------------------" ; broadcast
-
-	message="Final System Tweaks:" ; broadcast
-
-	unset root_uuid
-	root_uuid=$(/sbin/blkid -c /dev/null -s UUID -o value ${destination}p${media_rootfs})
-	if [ "${root_uuid}" ] ; then
-		sed -i -e 's:uuid=:#uuid=:g' /tmp/rootfs/boot/uEnv.txt
-		echo "uuid=${root_uuid}" >> /tmp/rootfs/boot/uEnv.txt
-
-		message="UUID=${root_uuid}" ; broadcast
-		root_uuid="UUID=${root_uuid}"
-	else
-		#really a failure...
-		root_uuid="${source}p${media_rootfs}"
-	fi
-
-	message="Generating: /etc/fstab" ; broadcast
-	echo "# /etc/fstab: static file system information." > /tmp/rootfs/etc/fstab
-	echo "#" >> /tmp/rootfs/etc/fstab
-	echo "${root_uuid}  /  ext4  noatime,errors=remount-ro  0  1" >> /tmp/rootfs/etc/fstab
-	echo "debugfs  /sys/kernel/debug  debugfs  defaults  0  0" >> /tmp/rootfs/etc/fstab
-	cat /tmp/rootfs/etc/fstab
-
-	message="/boot/uEnv.txt: disabling eMMC flasher script" ; broadcast
-	sed -i -e 's:'$emmcscript':#'$emmcscript':g' /tmp/rootfs/boot/uEnv.txt
-	cat /tmp/rootfs/boot/uEnv.txt
-	message="-----------------------------" ; broadcast
-
-	flush_cache
-	message="running: chroot /tmp/rootfs/ /usr/bin/bb-wl18xx-wlan0" ; broadcast
-
+  #FIXME: What about when you boot from a fat partition /boot ?
+  echo_broadcast "==> /boot/uEnv.txt: disabling eMMC flasher script"
+  sed -i -e 's:'$emmcscript':#'$emmcscript':g' ${tmp_rootfs_dir}/boot/uEnv.txt
+  generate_line 40 '*'
+  cat ${tmp_rootfs_dir}/boot/uEnv.txt
+  generate_line 40 '*'
+  flush_cache
+	echo_broadcast "==> running: chroot /tmp/rootfs/ /usr/bin/bb-wl18xx-wlan0"
+  echo_broadcast "==> bind some mount points"
 	mount --bind /proc /tmp/rootfs/proc
 	mount --bind /sys /tmp/rootfs/sys
 	mount --bind /dev /tmp/rootfs/dev
 	mount --bind /dev/pts /tmp/rootfs/dev/pts
-
+  echo_broadcast "==> Load module wl18xx"
 	modprobe wl18xx
-	message="-----------------------------" ; broadcast
-	message="lsmod" ; broadcast
-	message="`lsmod`" ; broadcast
-	message="-----------------------------" ; broadcast
+  generate_line 40
+	echo_broadcast "====> lsmod"
+	echo_broadcast "`lsmod`"
+  echo_broadcast "==> Chroot"
+  generate_line 40
 	chroot /tmp/rootfs/ /usr/bin/bb-wl18xx-wlan0
-	message="-----------------------------" ; broadcast
+  generate_line 40
 
 	flush_cache
-	message="initrd: `ls -lh /tmp/rootfs/boot/initrd.img*`" ; broadcast
-
+  echo_broadcast "==> initrd: $(ls -lh /tmp/rootfs/boot/initrd.img*)"
+  echo_broadcast "==> Unmount previous bind"
 	umount -fl /tmp/rootfs/dev/pts
 	umount -fl /tmp/rootfs/dev
 	umount -fl /tmp/rootfs/proc
@@ -128,52 +108,8 @@ copy_rootfs () {
 	sleep 2
 
 	flush_cache
-	message="-----------------------------" ; broadcast
-	umount /tmp/rootfs/ || umount -l /tmp/rootfs/ || write_failure
-
-	if [ "x${is_bbb}" = "xenable" ] ; then
-		[ -e /proc/$CYLON_PID ]  && kill $CYLON_PID
-	fi
-
-	message="Syncing: ${destination}" ; broadcast
-	#https://github.com/beagleboard/meta-beagleboard/blob/master/contrib/bone-flash-tool/emmc.sh#L158-L159
-	# force writeback of eMMC buffers
-	sync
-	dd if=${destination} of=/dev/null count=100000
-	message="Syncing: ${destination} complete" ; broadcast
-	message="-----------------------------" ; broadcast
-
-	if [ -f /boot/debug.txt ] ; then
-		message="This script has now completed its task" ; broadcast
-		message="-----------------------------" ; broadcast
-		message="debug: enabled" ; broadcast
-		inf_loop
-	else
-		umount /tmp || umount -l /tmp
-		if [ "x${is_bbb}" = "xenable" ] ; then
-			if [ -e /sys/class/leds/beaglebone\:green\:usr0/trigger ] ; then
-				echo default-on > /sys/class/leds/beaglebone\:green\:usr0/trigger
-				echo default-on > /sys/class/leds/beaglebone\:green\:usr1/trigger
-				echo default-on > /sys/class/leds/beaglebone\:green\:usr2/trigger
-				echo default-on > /sys/class/leds/beaglebone\:green\:usr3/trigger
-			fi
-		fi
-		mount
-
-		message="eMMC has been flashed: please wait for device to power down." ; broadcast
-		message="-----------------------------" ; broadcast
-
-		flush_cache
-		#To properly shudown, /opt/scripts/boot/am335x_evm.sh is going to call halt:
-		exec /sbin/init
-	fi
+  generate_line 80
 }
 
-sleep 5
-startup_message
-get_device
-check_eeprom
-check_running_system
-activate_cylon_leds
-partition_drive
-#
+prepare_drive
+
