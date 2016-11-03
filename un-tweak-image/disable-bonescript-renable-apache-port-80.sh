dbcc33029ce62304499a792772571ee1c8aea05d
Stephane Charette <stephanecharette@gmail.com>
2016-11-03 16:28:35 -0700

    fixed typo in apache conf

--- un-tweak-image/disable-bonescript-renable-apache-port-80.sh
+++ un-tweak-image/disable-bonescript-renable-apache-port-80.sh
@@ -2,5 +2,5 @@
 
 sudo systemctl disable bonescript.socket
 sudo sed -i -e 's:8080:80:g' /etc/apache2/ports.conf
-sudo sed -i -e 's:8080:80:g' /etc/apache2/sites-enabled/000-default
+sudo sed -i -e 's:8080:80:g' /etc/apache2/sites-enabled/000-default.conf
 sudo /etc/init.d/apache2 restart
