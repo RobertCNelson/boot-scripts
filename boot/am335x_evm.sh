157bde15fa73a8b79c1c0985a39eaa4753367d4a
Robert Nelson <robertcnelson@gmail.com>
2017-01-05 11:46:24 -0600

    am335x: libcomposite: we need to mount the mass stoage file
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- boot/am335x_evm.sh
+++ boot/am335x_evm.sh
@@ -368,6 +368,19 @@ if [ -f /var/run/udhcpd.pid ] ; then
 fi
 
 use_libcomposite () {
+	unset has_img_file
+	if [ -f ${usb_image_file} ] ; then
+		actual_image_file=$(readlink -f ${usb_image_file} || true)
+		if [ ! "x${actual_image_file}" = "x" ] ; then
+			if [ -f ${actual_image_file} ] ; then
+				has_img_file="true"
+				mkdir /tmp/usb_img/
+				losetup -v -f ${actual_image_file}
+				partx -v --add /dev/loop0
+				mount -t vfat -o ro /dev/loop0p1 /tmp/usb_img/
+			fi
+		fi
+	fi
 	modprobe libcomposite || true
 	if [ -d /sys/module/libcomposite ] ; then
 		if [ -d ${usb_gadget} ] ; then
@@ -399,14 +412,14 @@ use_libcomposite () {
 				echo ${cpsw_1_mac} > functions/rndis.usb0/dev_addr
 				mkdir -p functions/acm.usb0
 
-				if [ -f ${usb_image_file} ] ; then
+				if [ "x${has_img_file}" = "xtrue" ] ; then
 					mkdir -p functions/mass_storage.usb0
 					echo ${usb_ms_stall} > functions/mass_storage.0/stall
 					echo ${usb_ms_cdrom} > functions/mass_storage.0/lun.0/cdrom
-					echo ${usb_image_file} > functions/mass_storage.0/lun.0/file
 					echo ${usb_ms_nofua} > functions/mass_storage.0/lun.0/nofua
 					echo ${usb_ms_removable} > functions/mass_storage.0/lun.0/removable
 					echo ${usb_ms_ro} > functions/mass_storage.0/lun.0/ro
+					echo /tmp/usb_img/ > functions/mass_storage.0/lun.0/file
 				fi
 
 				mkdir -p configs/c.1/strings/0x409
@@ -416,7 +429,7 @@ use_libcomposite () {
 
 				ln -s functions/rndis.usb0 configs/c.1/
 				ln -s functions/acm.usb0 configs/c.1/
-				if [ -f ${usb_image_file} ] ; then
+				if [ "x${has_img_file}" = "xtrue" ] ; then
 					ln -s functions/mass_storage.usb0 configs/c.1/
 				fi
 
