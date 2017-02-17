e0b144f64e426db71ab7757cbfd87ce67c7aff2a
Robert Nelson <robertcnelson@gmail.com>
2017-02-17 17:06:39 -0600

    version: fixup u-boot
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/version.sh
+++ tools/version.sh
@@ -11,17 +11,19 @@ if [ -f /etc/dogtag ] ; then
 fi
 
 if [ -b /dev/mmcblk0 ] ; then
-	#U-Boot
-	dd if=/dev/mmcblk0 count=6  skip=393248 bs=1 2>/dev/null
-	#U-Boot 2017.03-rc1-00003-gad8008
-	dd if=/dev/mmcblk0 count=32 skip=393248 bs=8 2>/dev/null
+	drive=/dev/mmcblk0
+	test=$(dd if=${drive} count=6  skip=393248 bs=1 2>/dev/null || true)
+	if [ "x${test}" = "xU-Boot" ] ; then
+		dd if=${drive} count=32 skip=393248 bs=8 2>/dev/null
+	fi
 fi
 
 if [ -b /dev/mmcblk1 ] ; then
-        #U-Boot
-        dd if=/dev/mmcblk0 count=6  skip=393248 bs=1 2>/dev/null
-        #U-Boot 2017.03-rc1-00003-gad8008
-        dd if=/dev/mmcblk1 count=32 skip=393248 bs=8 2>/dev/null
+	drive=/dev/mmcblk1
+	test=$(dd if=${drive} count=6  skip=393248 bs=1 2>/dev/null || true)
+	if [ "x${test}" = "xU-Boot" ] ; then
+		dd if=${drive} count=32 skip=393248 bs=8 2>/dev/null
+	fi
 fi
 
 uname -r
