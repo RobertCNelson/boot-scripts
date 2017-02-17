59138cdde74621ad13d333583681017e2e5a542f
Robert Nelson <robertcnelson@gmail.com>
2017-02-17 17:10:28 -0600

    version: add new line
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/version.sh
+++ tools/version.sh
@@ -15,6 +15,7 @@ if [ -b /dev/mmcblk0 ] ; then
 	test=$(dd if=${drive} count=6  skip=393248 bs=1 2>/dev/null || true)
 	if [ "x${test}" = "xU-Boot" ] ; then
 		dd if=${drive} count=32 skip=393248 bs=1 2>/dev/null
+		echo ""
 	fi
 fi
 
@@ -23,6 +24,7 @@ if [ -b /dev/mmcblk1 ] ; then
 	test=$(dd if=${drive} count=6  skip=393248 bs=1 2>/dev/null || true)
 	if [ "x${test}" = "xU-Boot" ] ; then
 		dd if=${drive} count=32 skip=393248 bs=1 2>/dev/null
+		echo ""
 	fi
 fi
 
