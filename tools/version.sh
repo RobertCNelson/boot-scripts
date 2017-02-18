264e43494e118b34b37aa859265728a3b2bc8aac
Robert Nelson <robertcnelson@gmail.com>
2017-02-17 21:33:38 -0600

    version.sh: unify bootloader
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/version.sh
+++ tools/version.sh
@@ -26,7 +26,7 @@ if [ -b /dev/mmcblk0 ] ; then
 	test=$(dd if=${drive} count=6  skip=393248 bs=1 2>/dev/null || true)
 	if [ "x${test}" = "xU-Boot" ] ; then
 		uboot=$(dd if=${drive} count=32 skip=393248 bs=1 2>/dev/null || true)
-		echo "bootloader on [${drive}]:[${uboot}]"
+		echo "bootloader:[${drive}]:[${uboot}]"
 	fi
 fi
 
@@ -35,7 +35,7 @@ if [ -b /dev/mmcblk1 ] ; then
 	test=$(dd if=${drive} count=6  skip=393248 bs=1 2>/dev/null || true)
 	if [ "x${test}" = "xU-Boot" ] ; then
 		uboot=$(dd if=${drive} count=32 skip=393248 bs=1 2>/dev/null || true)
-		echo "bootloader on [${drive}]:[${uboot}]"
+		echo "bootloader:[${drive}]:[${uboot}]"
 	fi
 fi
 
