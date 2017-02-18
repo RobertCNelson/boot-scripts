931ae794091c3ea32ca2db12a39ce58992dfc4c3
Robert Nelson <robertcnelson@gmail.com>
2017-02-17 21:29:26 -0600

    version.sh: add git version of /opt/scripts and nodejs version
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/version.sh
+++ tools/version.sh
@@ -6,16 +6,26 @@ if ! id | grep -q root; then
 	exit
 fi
 
+git_bin=$(which git)
+
+if [ -f ${git_bin} ] ; then
+	if [ -d /opt/scripts/ ] ; then
+		cd /opt/scripts/
+		echo "git:/opt/scripts/:[$(${git_bin} rev-parse HEAD)]"
+		cd -
+	fi
+fi
+
 if [ -f /etc/dogtag ] ; then
-	cat /etc/dogtag
+	echo "dogtag:[`cat /etc/dogtag`]"
 fi
 
 if [ -b /dev/mmcblk0 ] ; then
 	drive=/dev/mmcblk0
 	test=$(dd if=${drive} count=6  skip=393248 bs=1 2>/dev/null || true)
 	if [ "x${test}" = "xU-Boot" ] ; then
-		dd if=${drive} count=32 skip=393248 bs=1 2>/dev/null
-		echo ""
+		uboot=$(dd if=${drive} count=32 skip=393248 bs=1 2>/dev/null || true)
+		echo "bootloader on ${drive}:[${uboot}]"
 	fi
 fi
 
@@ -23,10 +33,13 @@ if [ -b /dev/mmcblk1 ] ; then
 	drive=/dev/mmcblk1
 	test=$(dd if=${drive} count=6  skip=393248 bs=1 2>/dev/null || true)
 	if [ "x${test}" = "xU-Boot" ] ; then
-		dd if=${drive} count=32 skip=393248 bs=1 2>/dev/null
-		echo ""
+		uboot=$(dd if=${drive} count=32 skip=393248 bs=1 2>/dev/null || true)
+		echo "bootloader on ${drive}:[${uboot}]"
 	fi
 fi
 
-uname -r
+echo "kernel:[`uname -r`]"
 
+if [ -f /usr/bin/nodejs ] ; then
+	echo "nodejs:[`/usr/bin/nodejs --version`]"
+fi
