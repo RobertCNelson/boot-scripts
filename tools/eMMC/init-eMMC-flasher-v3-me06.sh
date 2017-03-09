bb94d7b04a5330b03963b7f50fd906c15ab558d1
Robert Nelson <robertcnelson@gmail.com>
2017-03-09 10:12:28 -0600

    add me06
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ tools/eMMC/init-eMMC-flasher-v3-me06.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2013-2016 Robert Nelson <robertcnelson@gmail.com>
+# Portions copyright (c) 2014 Charles Steinkuehler <charles@steinkuehler.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+#This script assumes, these packages are installed, as network may not be setup
+#dosfstools initramfs-tools rsync u-boot-tools
+
+source $(dirname "$0")/functions.sh
+
+#mke2fs -c
+#Check the device for bad blocks before creating the file system.
+#If this option is specified twice, then a slower read-write test is
+#used instead of a fast read-only test.
+
+mkfs_options=""
+#mkfs_options="-c"
+#mkfs_options="-cc"
+
+device_eeprom="me06-eeprom"
+
+check_if_run_as_root
+
+startup_message
+prepare_environment
+
+countdown 5
+check_eeprom
+check_running_system
+activate_cylon_leds
+prepare_drive
+#
