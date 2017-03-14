f616fbf57c6b9b926bd7d4ae8cff9e634a29c854
Robert Nelson <robertcnelson@gmail.com>
2017-03-14 10:47:07 -0500

    eewiki: use v2017.03-r10
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/eMMC/bbb-eMMC-flasher-eewiki-ext4.sh
+++ tools/eMMC/bbb-eMMC-flasher-eewiki-ext4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright (c) 2013-2016 Robert Nelson <robertcnelson@gmail.com>
+# Copyright (c) 2013-2017 Robert Nelson <robertcnelson@gmail.com>
 # Portions copyright (c) 2014 Charles Steinkuehler <charles@steinkuehler.net>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -24,11 +24,11 @@
 #This script assumes, these packages are installed, as network may not be setup
 #dosfstools initramfs-tools rsync u-boot-tools
 
-version_message="1.20161130: u-boot v2016.11..."
+version_message="1.20170314: u-boot v2017.03..."
 #
 #https://rcn-ee.com/repos/bootloader/am335x_evm/
-http_spl="MLO-am335x_evm-v2016.11-r2"
-http_uboot="u-boot-am335x_evm-v2016.11-r2.img"
+http_spl="MLO-am335x_evm-v2017.03-r10"
+http_uboot="u-boot-am335x_evm-v2017.03-r10.img"
 
 #mke2fs -c
 #Check the device for bad blocks before creating the file system.
