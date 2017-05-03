8557455e9fe4fba4e6da108c991344806595783a
Robert Nelson <robertcnelson@gmail.com>
2017-05-03 15:29:01 -0500

    update_kernel.sh: bone-v4.11.x has sgx modules
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/update_kernel.sh
+++ tools/update_kernel.sh
@@ -525,6 +525,13 @@ third_party () {
 				run_depmod_initramfs="enabled"
 			fi
 			;;
+		TESTING)
+			#v4.11.x sgx modules are working again...
+			if [ "x${es8}" = "xenabled" ] ; then
+				apt-get ${apt_options} ti-sgx-es8-modules-${latest_kernel} || true
+				run_depmod_initramfs="enabled"
+			fi
+			;;
 		esac
 		;;
 	ti-xenomai)
