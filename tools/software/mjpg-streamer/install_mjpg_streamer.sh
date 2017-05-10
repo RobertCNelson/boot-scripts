2f5f8db1a4c0ef5c38c010789c2beb11c4229aa7
Robert Nelson <robertcnelson@gmail.com>
2017-05-10 15:45:01 -0500

    mjpg-streamer: add chmod values
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/software/mjpg-streamer/install_mjpg_streamer.sh
+++ tools/software/mjpg-streamer/install_mjpg_streamer.sh
@@ -12,35 +12,15 @@ uname -a
 lsusb
 # Bus 001 Device 002: ID 046d:0825 Logitech, Inc. Webcam C270
 # Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub
-v4l2-ctl --info
-# Driver Info (not using libv4l2):
-#         Driver name   : uvcvideo
-#         Card type     : UVC Camera (046d:0825)
-#         Bus info      : usb-musb-hdrc.1.auto-1
-#         Driver version: 4.4.41
-#         Capabilities  : 0x84200001
-#                 Video Capture
-#                 Streaming
-#                 Extended Pix Format
-#                 Device Capabilities
-#         Device Caps   : 0x04200001
-#                 Video Capture
-#                 Streaming
-#                 Extended Pix Format
-#apt-get install -y libv4l-dev libjpeg-dev imagemagick subversion
-#svn co https://svn.code.sf.net/p/mjpg-streamer/code/mjpg-streamer mjpg-streamer
-# SVN_REV="3:172"
-#cd mjpg-streamer
-#make
-#./mjpg_streamer ./mjpg_streamer -i "./input_uvc.so -yuv" -o "./output_http.so -p 8090 -w ./www"
-#make install
-#mjpg_streamer -i "/usr/local/lib/input_uvc.so -yuv" -o "/usr/local/lib/output_http.so -p 8090 -w /usr/local/www"
+if [ -d /dev/video0 ] ; then
+	v4l2-ctl --info
+fi
 
 if [ ! -f /usr/bin/mjpg_streamer ] ; then
 	apt update
 	apt install -y mjpg-streamer
 fi
 
-install -m ./mjpg-streamer.rules /etc/udev/rules.d
-install -m ./mjpg-streamer.service /etc/systemd/system
+install -m 644 ./mjpg-streamer.rules /etc/udev/rules.d
+install -m 644 ./mjpg-streamer.service /etc/systemd/system
 systemctl restart mjpg-streamer
