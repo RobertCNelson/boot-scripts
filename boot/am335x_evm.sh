09ae22ec483e5483c6ae7f0ca7bbbabf864b06af
Robert Nelson <robertcnelson@gmail.com>
2017-06-12 13:57:36 -0500

    am335x_evm.sh: this is no-no, especially when connman isnt installed
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- boot/am335x_evm.sh
+++ boot/am335x_evm.sh
@@ -700,7 +700,8 @@ if [ "x${dnsmasq_usb0_usb1}" = "xenabled" ] ; then
 			echo "dhcp-option=usb0,6" >> ${wfile}
 			echo "dhcp-option=usb1,3" >> ${wfile}
 			echo "dhcp-option=usb1,6" >> ${wfile}
-			echo "address=/#/172.1.8.1" >> ${wfile}
+#FIXME: why was this added, without connman every ip get's 172.1.8.1????
+#			echo "address=/#/172.1.8.1" >> ${wfile}
 			echo "dhcp-leasefile=/var/run/dnsmasq.leases" >> ${wfile}
 
 			systemctl restart dnsmasq || true
