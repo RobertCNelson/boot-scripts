02868ba533a9e4b946191f58a7fa41b968220c60
Robert Nelson <robertcnelson@gmail.com>
2018-01-27 16:00:06 -0600

    dump user groups
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/version.sh
+++ tools/version.sh
@@ -128,6 +128,8 @@ pkg="bb-cape-overlays" ; dpkg_check_version
 pkg="bb-wl18xx-firmware" ; dpkg_check_version
 pkg="firmware-ti-connectivity" ; dpkg_check_version
 
+echo "User Belongs to Groups:[`groups`]"
+
 echo "dmesg | grep pinctrl-single"
 dmesg | grep pinctrl-single
 echo "END"
