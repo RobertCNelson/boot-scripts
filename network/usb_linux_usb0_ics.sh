fe782b85e53e7488a7e7667585395892f4195567
Robert Nelson <robertcnelson@gmail.com>
2018-09-14 11:11:37 -0500

    add internel network setup scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ network/usb_linux_usb0_ics.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+#
+
+if ! id | grep -q root; then
+	echo "must be run as root"
+	exit
+fi
+
+/sbin/route add default gw 192.168.7.1
+
+ping -c1 8.8.8.8
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+#
---
+++ network/usb_mac_ics.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+#
+
+if ! id | grep -q root; then
+	echo "must be run as root"
+	exit
+fi
+
+/sbin/route add default gw 192.168.6.1
+
+ping -c1 8.8.8.8
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+#
