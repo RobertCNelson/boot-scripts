288a143ff1aaeb8f6df5c328b60b6d220082cd47
Robert Nelson <robertcnelson@gmail.com>
2018-12-13 16:14:25 -0600

    developers: add .local to apt-proxy dns...
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/developers/apt-proxy.sh
+++ tools/developers/apt-proxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo "Acquire::http::Proxy \"http://apt-proxy:3142/\";" > /tmp/apt.conf
+echo "Acquire::http::Proxy \"http://apt-proxy.local:3142/\";" > /tmp/apt.conf
 echo "Acquire::PDiffs \"false\";" >> /tmp/apt.conf
 sudo cp -v /tmp/apt.conf /etc/apt/apt.conf
 
