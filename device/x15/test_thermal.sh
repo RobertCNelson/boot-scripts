ba9baa7f3e182646cd11b96a7728b0fbe06aabf1
Robert Nelson <robertcnelson@gmail.com>
2019-02-12 12:46:12 -0600

    x15: fix thermal
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/x15/test_thermal.sh
+++ device/x15/test_thermal.sh
@@ -7,50 +7,52 @@ echo -e "|Thermal zone\t\t|temp\t|mode\t\t|cdev0_type\t|cdev1_type"
 echo -e "------------------------------------------------------------------------------------------------"
 for i in `seq 0 5`
 do
-	type=$(cat ${base}/thermal_zone$i/type)
-	temp=$(cat ${base}/thermal_zone$i/temp)
-	mode=$(cat ${base}/thermal_zone$i/mode)
+	if [ -d ${base}/thermal_zone$i/ ] ; then
+		type=$(cat ${base}/thermal_zone$i/type)
+		temp=$(cat ${base}/thermal_zone$i/temp)
+		mode=$(cat ${base}/thermal_zone$i/mode)
 
-	if [ -d ${base}/thermal_zone$i/cdev0 ] ; then
-		cdev0_type=$(cat ${base}/thermal_zone$i/cdev0/type)
-	else
-		cdev0_type="\t"
-	fi
+		if [ -d ${base}/thermal_zone$i/cdev0 ] ; then
+			cdev0_type=$(cat ${base}/thermal_zone$i/cdev0/type)
+		else
+			cdev0_type="\t"
+		fi
 
-	if [ -d ${base}/thermal_zone$i/cdev1 ] ; then
-		cdev1_type=$(cat ${base}/thermal_zone$i/cdev1/type)
-	else
-		cdev1_type="\t"
-	fi
+		if [ -d ${base}/thermal_zone$i/cdev1 ] ; then
+			cdev1_type=$(cat ${base}/thermal_zone$i/cdev1/type)
+		else
+			cdev1_type="\t"
+		fi
 
-	if [ -f ${base}/thermal_zone$i/trip_point_0_temp ] ; then
-		trip_point_0_temp=$(cat ${base}/thermal_zone$i/trip_point_0_temp)
-		trip_point_0_type=$(cat ${base}/thermal_zone$i/trip_point_0_type)
-		trip_point_0="${trip_point_0_type}:${trip_point_0_temp}|"
-	else
-		trip_point_0=""
-	fi
+		if [ -f ${base}/thermal_zone$i/trip_point_0_temp ] ; then
+			trip_point_0_temp=$(cat ${base}/thermal_zone$i/trip_point_0_temp)
+			trip_point_0_type=$(cat ${base}/thermal_zone$i/trip_point_0_type)
+			trip_point_0="${trip_point_0_type}:${trip_point_0_temp}|"
+		else
+			trip_point_0=""
+		fi
 
-	if [ -f ${base}/thermal_zone$i/trip_point_1_temp ] ; then
-		trip_point_1_temp=$(cat ${base}/thermal_zone$i/trip_point_1_temp)
-		trip_point_1_type=$(cat ${base}/thermal_zone$i/trip_point_1_type)
-		trip_point_1="${trip_point_1_type}:${trip_point_1_temp}|"
-	else
-		trip_point_0=""
-	fi
+		if [ -f ${base}/thermal_zone$i/trip_point_1_temp ] ; then
+			trip_point_1_temp=$(cat ${base}/thermal_zone$i/trip_point_1_temp)
+			trip_point_1_type=$(cat ${base}/thermal_zone$i/trip_point_1_type)
+			trip_point_1="${trip_point_1_type}:${trip_point_1_temp}|"
+		else
+			trip_point_0=""
+		fi
 
-	if [ -f ${base}/thermal_zone$i/trip_point_2_temp ] ; then
-		trip_point_2_temp=$(cat ${base}/thermal_zone$i/trip_point_2_temp)
-		trip_point_2_type=$(cat ${base}/thermal_zone$i/trip_point_2_type)
-		trip_point_2="${trip_point_2_type}:${trip_point_2_temp}|"
-	else
-		trip_point_2=""
-	fi
+		if [ -f ${base}/thermal_zone$i/trip_point_2_temp ] ; then
+			trip_point_2_temp=$(cat ${base}/thermal_zone$i/trip_point_2_temp)
+			trip_point_2_type=$(cat ${base}/thermal_zone$i/trip_point_2_type)
+			trip_point_2="${trip_point_2_type}:${trip_point_2_temp}|"
+		else
+			trip_point_2=""
+		fi
 
-	if [ "x${cdev1_type}" = "xthermal-cpufreq-0" ] ; then
-		echo -e "|${type}\t\t|${temp}\t|${mode}\t|${cdev0_type}\t|${cdev1_type}\t|${trip_point_0}${trip_point_1}${trip_point_2}"
-	else
-		echo -e "|${type}\t\t|${temp}\t|${mode}\t|${cdev0_type}\t|${cdev1_type}\t\t|${trip_point_0}${trip_point_1}${trip_point_2}"
+		if [ "x${cdev1_type}" = "xthermal-cpufreq-0" ] ; then
+			echo -e "|${type}\t\t|${temp}\t|${mode}\t|${cdev0_type}\t|${cdev1_type}\t|${trip_point_0}${trip_point_1}${trip_point_2}"
+		else
+			echo -e "|${type}\t\t|${temp}\t|${mode}\t|${cdev0_type}\t|${cdev1_type}\t\t|${trip_point_0}${trip_point_1}${trip_point_2}"
+		fi
 	fi
 done
 echo -e "------------------------------------------------------------------------------------------------"
