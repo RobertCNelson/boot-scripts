d807dc83150c951aa4f23513c2a1d70f957cb845
Robert Nelson <robertcnelson@gmail.com>
2019-02-12 12:51:49 -0600

    x15: fix thermal
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/x15/test_thermal.sh
+++ device/x15/test_thermal.sh
@@ -60,14 +60,16 @@ echo -e "|Cooling type\t\t|state\t|max_state\t|"
 echo -e "------------------------------------------------------------------------------------------------"
 for i in `seq 0 2`
 do
-	type=$(cat ${base}/cooling_device$i/type)
-	cur_state=$(cat ${base}/cooling_device$i/cur_state)
-	max_state=$(cat ${base}/cooling_device$i/max_state)
+	if [ -d ${base}/cooling_device$i/ ] ; then
+		type=$(cat ${base}/cooling_device$i/type)
+		cur_state=$(cat ${base}/cooling_device$i/cur_state)
+		max_state=$(cat ${base}/cooling_device$i/max_state)
 
-	if [ "x${type}" = "xthermal-cpufreq-0" ] ; then
-		echo -e "|${type}\t|${cur_state}\t|${max_state}\t\t|"
-	else
-		echo -e "|${type}\t\t|${cur_state}\t|${max_state}\t\t|"
+		if [ "x${type}" = "xthermal-cpufreq-0" ] ; then
+			echo -e "|${type}\t|${cur_state}\t|${max_state}\t\t|"
+		else
+			echo -e "|${type}\t\t|${cur_state}\t|${max_state}\t\t|"
+		fi
 	fi
 done
 echo -e "------------------------------------------------------------------------------------------------"
