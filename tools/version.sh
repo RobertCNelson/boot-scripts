9df8deb6b2ae7570c9102ab58f795d3bed592194
Robert Nelson <robertcnelson@gmail.com>
2019-04-07 11:25:18 -0500

    version: add warning about /uEnv.txt
    fixes: https://github.com/beagleboard/bb.org-overlays/issues/79
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/version.sh
+++ tools/version.sh
@@ -151,6 +151,9 @@ if [ -f /boot/uEnv.txt ] ; then
 	unset test_var
 	test_var=$(cat /boot/uEnv.txt | grep -v '#' | grep enable_uboot_overlays=1 || true)
 	if [ "x${test_var}" != "x" ] ; then
+		if [ -f /uEnv.txt ] ; then
+			echo "/uEnv.txt exists, uboot overlays is DISABLED, remove /uEnv.txt"
+		fi
 		cat /boot/uEnv.txt | grep uboot_ | grep -v '#' | sed 's/^/uboot_overlay_options:[/' | sed 's/$/]/'
 		cat /boot/uEnv.txt | grep dtb_overlay | grep -v '#' | sed 's/^/uboot_overlay_options:[/' | sed 's/$/]/'
 	fi
