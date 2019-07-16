a98c9c5901209b09cfcf3519dde25bd34bde957d
Robert Nelson <robertcnelson@gmail.com>
2019-07-16 13:45:13 -0500

    add buster defaults
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- desktop-defaults/buster/reset_lxqt.sh
+++ desktop-defaults/buster/reset_lxqt.sh
@@ -3,6 +3,6 @@
 rm -rf /home/${USER}/.config/lxqt || true
 rm -rf /home/${USER}/.config/pcmanfm-qt || true
 
-cp -rf /opt/scripts/desktop-defaults/stretch/lxqt/* /home/${USER}/.config
+cp -rf /opt/scripts/desktop-defaults/buster/lxqt/* /home/${USER}/.config
 
 sync
