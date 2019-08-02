e9ba2588fce46c29ecde24bede61dbba53c768cc
Robert Nelson <robertcnelson@gmail.com>
2019-08-02 12:45:58 -0500

    bbai: merge bbb gadget changes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- boot/bbai.sh
+++ boot/bbai.sh
@@ -192,23 +192,34 @@ run_libcomposite () {
 		echo ${usb_imanufacturer} > strings/0x409/manufacturer
 		echo ${usb_iproduct} > strings/0x409/product
 
-		mkdir -p functions/rndis.usb0
-		# first byte of address must be even
-		echo ${cpsw_1_mac} > functions/rndis.usb0/host_addr
-		echo ${cpsw_2_mac} > functions/rndis.usb0/dev_addr
+		if [ ! "x${USB_NETWORK_DISABLED}" = "xyes" ]; then
+			mkdir -p functions/rndis.usb0
+			# first byte of address must be even
+			echo ${cpsw_1_mac} > functions/rndis.usb0/host_addr
+			echo ${cpsw_2_mac} > functions/rndis.usb0/dev_addr
 
-		# Starting with kernel 4.14, we can do this to match Microsoft's built-in RNDIS driver.
-		# Earlier kernels require the patch below as a work-around instead:
-		# https://github.com/beagleboard/linux/commit/e94487c59cec8ba32dc1eb83900297858fdc590b
-		if [ -f functions/rndis.usb0/class ]; then
-			echo EF > functions/rndis.usb0/class
-			echo 04 > functions/rndis.usb0/subclass
-			echo 01 > functions/rndis.usb0/protocol
+			# Starting with kernel 4.14, we can do this to match Microsoft's built-in RNDIS driver.
+			# Earlier kernels require the patch below as a work-around instead:
+			# https://github.com/beagleboard/linux/commit/e94487c59cec8ba32dc1eb83900297858fdc590b
+			if [ -f functions/rndis.usb0/class ]; then
+				echo EF > functions/rndis.usb0/class
+				echo 04 > functions/rndis.usb0/subclass
+				echo 01 > functions/rndis.usb0/protocol
+			fi
+
+			# Add OS Descriptors for the latest Windows 10 rndiscmp.inf
+			# https://answers.microsoft.com/en-us/windows/forum/windows_10-networking-winpc/windows-10-vs-remote-ndis-ethernet-usbgadget-not/cb30520a-753c-4219-b908-ad3d45590447
+			# https://www.spinics.net/lists/linux-usb/msg107185.html
+			echo 1 > os_desc/use
+			echo CD > os_desc/b_vendor_code
+			echo MSFT100 > os_desc/qw_sign
+			echo "RNDIS" > functions/rndis.usb0/os_desc/interface.rndis/compatible_id
+			echo "5162001" > functions/rndis.usb0/os_desc/interface.rndis/sub_compatible_id
+
+			mkdir -p functions/ecm.usb0
+			echo ${cpsw_3_mac} > functions/ecm.usb0/host_addr
+			echo ${cpsw_4_mac} > functions/ecm.usb0/dev_addr
 		fi
-
-		mkdir -p functions/ecm.usb0
-		echo ${cpsw_3_mac} > functions/ecm.usb0/host_addr
-		echo ${cpsw_4_mac} > functions/ecm.usb0/dev_addr
 
 		mkdir -p functions/acm.usb0
 
@@ -228,8 +239,18 @@ run_libcomposite () {
 
 		echo 500 > configs/c.1/MaxPower
 
-		ln -s functions/rndis.usb0 configs/c.1/
-		ln -s functions/ecm.usb0 configs/c.1/
+		if [ ! "x${USB_NETWORK_DISABLED}" = "xyes" ]; then
+			ln -s configs/c.1 os_desc
+			mkdir functions/rndis.usb0/os_desc/interface.rndis/Icons
+			echo 2 > functions/rndis.usb0/os_desc/interface.rndis/Icons/type
+			echo "%SystemRoot%\\system32\\shell32.dll,-233" > functions/rndis.usb0/os_desc/interface.rndis/Icons/data
+			mkdir functions/rndis.usb0/os_desc/interface.rndis/Label
+			echo 1 > functions/rndis.usb0/os_desc/interface.rndis/Label/type
+			echo "BeagleBone USB Ethernet" > functions/rndis.usb0/os_desc/interface.rndis/Label/data
+
+			ln -s functions/rndis.usb0 configs/c.1/
+			ln -s functions/ecm.usb0 configs/c.1/
+		fi
 		ln -s functions/acm.usb0 configs/c.1/
 		if [ "x${has_img_file}" = "xtrue" ] ; then
 			ln -s functions/mass_storage.usb0 configs/c.1/
