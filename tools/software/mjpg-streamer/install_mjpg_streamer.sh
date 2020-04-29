3d5e64e8c6371494ed9a7d73de4b933246aba749
Robert Nelson <robertcnelson@gmail.com>
2020-04-29 10:49:24 -0500

    mjpg, make sure we have everything
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/software/mjpg-streamer/install_mjpg_streamer.sh
+++ tools/software/mjpg-streamer/install_mjpg_streamer.sh
@@ -9,11 +9,15 @@ cat /etc/dogtag
 # BeagleBoard.org Debian Image 2017-01-17
 uname -a
 # Linux blue-dc5d 4.4.41-ti-r83 #1 SMP Tue Jan 17 00:01:19 UTC 2017 armv7l GNU/Linux
-lsusb
+if [ -f /usr/bin/lsusb ] ; then
+	lsusb
+fi
 # Bus 001 Device 002: ID 046d:0825 Logitech, Inc. Webcam C270
 # Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub
-if [ -d /dev/video0 ] ; then
-	v4l2-ctl --info
+if [ -f /usr/bin/v4l2-ctl ] ; then
+	if [ -d /dev/video0 ] ; then
+		v4l2-ctl --info
+	fi
 fi
 
 if [ ! -f /usr/bin/mjpg_streamer ] ; then
