c9fc4d88de79a75c0c5fd1cc1f7b91ee3ba158fe
Robert Nelson <robertcnelson@gmail.com>
2020-05-15 11:19:07 -0500

    generator grab cmdline
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/generate_extlinux.sh
+++ tools/generate_extlinux.sh
@@ -10,15 +10,13 @@ if [ ! -d /boot/extlinux/ ] ; then
 	mkdir /boot/extlinux/ || true
 fi
 
-uname_r=$(cat /boot/uEnv.txt  | grep -v "#" | grep uname_r)
+uname_r=$(cat /boot/uEnv.txt | grep -v '#' | sed 's/ /\n/g' | grep 'uname_r=' | awk -F"=" '{print $2}' || true)
+cmdline=$(cat /boot/uEnv.txt | grep -v '#' | sed 's/ /\n/g' | grep 'cmdline=' | awk -F"=" '{print $2}' || true)
 
-#label Linux 4.14.108-ti-r131
-#    kernel /boot/vmlinuz-4.14.108-ti-r131
-#    append console=ttyO0,115200n8 root=/dev/mmcblk0p1 ro rootfstype=ext4 rootwait coherent_pool=1M net.ifnames=0 lpj=1990656 rng_core.default_quality=100
-#    fdtdir /boot/dtbs/4.14.108-ti-r131/
-
-echo "label Linux ${uname_r}"
-echo "    kernel /boot/vmlinuz-${uname_r}"
-echo "    append"
-echo "    fdtdir /boot/dtbs/${uname_r}/"
+if [ ! "x${uname_r}" = "x"] && [ ! "x${cmdline}" = "x" ] ; then
+	echo "label Linux ${uname_r}"
+	echo "    kernel /boot/vmlinuz-${uname_r}"
+	echo "    append ${cmdline}"
+	echo "    fdtdir /boot/dtbs/${uname_r}/"
+fi
 
