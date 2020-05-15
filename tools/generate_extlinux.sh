58a032ab95dcf7a1b110e2da8c839d77cdde2e8d
Robert Nelson <robertcnelson@gmail.com>
2020-05-15 11:19:57 -0500

    generate: fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/generate_extlinux.sh
+++ tools/generate_extlinux.sh
@@ -13,10 +13,12 @@ fi
 uname_r=$(cat /boot/uEnv.txt | grep -v '#' | sed 's/ /\n/g' | grep 'uname_r=' | awk -F"=" '{print $2}' || true)
 cmdline=$(cat /boot/uEnv.txt | grep -v '#' | sed 's/ /\n/g' | grep 'cmdline=' | awk -F"=" '{print $2}' || true)
 
-if [ ! "x${uname_r}" = "x"] && [ ! "x${cmdline}" = "x" ] ; then
-	echo "label Linux ${uname_r}"
-	echo "    kernel /boot/vmlinuz-${uname_r}"
-	echo "    append ${cmdline}"
-	echo "    fdtdir /boot/dtbs/${uname_r}/"
+if [ ! "x${uname_r}" = "x" ] ; then
+	if [ ! "x${cmdline}" = "x" ] ; then
+		echo "label Linux ${uname_r}"
+		echo "    kernel /boot/vmlinuz-${uname_r}"
+		echo "    append ${cmdline}"
+		echo "    fdtdir /boot/dtbs/${uname_r}/"
+	fi
 fi
 
