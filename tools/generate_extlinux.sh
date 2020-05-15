efee50283dbda781b87a030205072ff39bb4ca81
Robert Nelson <robertcnelson@gmail.com>
2020-05-15 11:23:20 -0500

    generate: strip start of cmdline
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/generate_extlinux.sh
+++ tools/generate_extlinux.sh
@@ -10,15 +10,16 @@ if [ ! -d /boot/extlinux/ ] ; then
 	mkdir /boot/extlinux/ || true
 fi
 
-uname_r=$(cat /boot/uEnv.txt | grep -v '#' | sed 's/ /\n/g' | grep 'uname_r=' | awk -F"=" '{print $2}' || true)
-cmdline=$(cat /boot/uEnv.txt | grep -v '#' | sed 's/ /\n/g' | grep 'cmdline=' | awk -F"=" '{print $2}' || true)
+var_uname_r=$(cat /boot/uEnv.txt | grep -v '#' | sed 's/ /\n/g' | grep 'uname_r=' | awk -F"=" '{print $2}' || true)
+var_cmdline=$(cat /boot/uEnv.txt | grep -v '#' | grep 'cmdline=' || true)
+var_cmdline=${var_cmdline##*cmdline=}
 
-if [ ! "x${uname_r}" = "x" ] ; then
-	if [ ! "x${cmdline}" = "x" ] ; then
-		echo "label Linux ${uname_r}"
-		echo "    kernel /boot/vmlinuz-${uname_r}"
-		echo "    append ${cmdline}"
-		echo "    fdtdir /boot/dtbs/${uname_r}/"
+if [ ! "x${var_uname_r}" = "x" ] ; then
+	if [ ! "x${var_cmdline}" = "x" ] ; then
+		echo "label Linux ${var_uname_r}"
+		echo "    kernel /boot/vmlinuz-${var_uname_r}"
+		echo "    append ${var_cmdline}"
+		echo "    fdtdir /boot/dtbs/${var_uname_r}/"
 	fi
 fi
 
